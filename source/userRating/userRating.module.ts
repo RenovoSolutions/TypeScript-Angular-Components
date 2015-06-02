45beb64ba0a498cae81a943066a1263793ff1c8d
Josh Graber <jgraber@renovo1.com>
2015-06-02 11:50:29 -0400

    Initial commit with components

---
+++ source/userRating/userRating.module.ts
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+export var name: string = 'rl21.components.userRating';
+
+import __userRating = require('./userRating');
+
+angular.module(name, [])
+	.directive(__userRating.directiveName, __userRating.userRating)
+	.controller(__userRating.controllerName, __userRating.UserRatingController);
