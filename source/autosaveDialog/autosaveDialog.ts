2244192585fccb476c6d69d368a24e6af3de9f68
Josh Graber <jgraber@renovo1.com>
2015-06-02 11:50:29 -0400

    Initial commit with components

---
+++ source/autosaveDialog/autosaveDialog.ts
@@ -0,0 +1,95 @@
+﻿'use strict';
+
+export var directiveName: string = 'rlAutosaveDialog';
+export var controllerName: string = 'AutosaveDialogController';
+
+import __parentChildBehavior = require('../../services/parentChildBehavior/parentChildBehavior.service');
+import __autosaveComponent = require('../autosaveComponent/autosaveComponent');
+
+export interface IAutosaveDialogController {
+	isOpen: boolean;
+	cancel(): void;
+	saveAndClose(): void;
+	autosave(): boolean;
+	saveAction(): ng.IPromise<void>;
+	saveOnClose: boolean;
+	autosaveLink: __parentChildBehavior.IChild<__autosaveComponent.IAutosaveBehavior>;
+	explicit: boolean;
+}
+
+export interface ISaveParams {
+	explicit: boolean;
+}
+
+export interface IAutosaveDialogScope extends ng.IScope {
+	isOpen: boolean;
+	validate(): boolean;
+	save(params: ISaveParams): ng.IPromise<void>;
+}
+
+export class AutosaveDialogController implements IAutosaveDialogController {
+	static $inject: string[] = ['$scope', __parentChildBehavior.name];
+	constructor(private $scope: IAutosaveDialogScope
+			, private parentChildBehavior: __parentChildBehavior.IParentChildBehaviorService) {
+		this.autosaveLink = { viewData: null };
+
+		this.$scope.$watch('isOpen', (value: boolean): void => {
+			this.isOpen = value;
+			if (value) {
+				this.saveOnClose = true;
+			}
+		});
+
+		this.$scope.$watch((): boolean => { return this.isOpen; }, (value: boolean): void => {
+			this.$scope.isOpen = value;
+		});
+	}
+
+	autosaveLink: __parentChildBehavior.IChild<__autosaveComponent.IAutosaveBehavior>;
+	isOpen: boolean;
+	explicit: boolean = false;
+	saveOnClose: boolean;
+
+	cancel(): void {
+		this.saveOnClose = false;
+		this.isOpen = false;
+	}
+
+	saveAndClose(): void {
+		this.saveOnClose = true;
+		this.explicit = true;
+		this.isOpen = false;
+	}
+
+	autosave: {(): boolean} = (): boolean => {
+		if (this.saveOnClose === false) {
+			return true;
+		} else {
+			var behavior: __autosaveComponent.IAutosaveBehavior = this.parentChildBehavior.getChildBehavior(this.autosaveLink);
+			return behavior.autosave();
+		}
+	}
+
+	saveAction(): ng.IPromise<void> {
+		var params: ISaveParams = { explicit: this.explicit };
+		this.explicit = false;
+		return this.$scope.save(params);
+	}
+}
+
+export class AutosaveDialog implements ng.IDirective {
+	static instance(): ng.IDirective {
+		return new AutosaveDialog();
+	}
+
+	restrict: string = 'E';
+	transclude: boolean = true;
+	templateUrl: string = 'components/autosaveDialog/autosaveDialog.html';
+	controller: string = controllerName;
+	controllerAs: string = 'dialog';
+	scope: any = {
+		isOpen: '=',
+		validate: '&',
+		save: '&',
+	};
+}
---
+++ source/userRating/userRating.tests.ts
@@ -0,0 +1,68 @@
+﻿/// <reference path='../../../typings/chai/chai.d.ts' />
+/// <reference path='../../../typings/mocha/mocha.d.ts' />
+/// <reference path='../../typings/angularMocks.d.ts' />
+/// <reference path='../../typings/chaiAssertions.d.ts' />
+
+import __userRatingModule = require('./userRating.module');
+import __userRating = require('./userRating');
+import __angularFixture = require('../../test/angularFixture');
+
+interface INgModelMock {
+	$setViewValue(value: number): void;
+	$viewValue: number;
+}
+
+describe('UserRatingController', () => {
+	var scope: __userRating.IUserRatingScope;
+	var userRating: __userRating.IUserRatingController;
+
+	beforeEach(() => {
+		angular.mock.module(__userRatingModule.name);
+
+		buildController();
+	});
+
+	it('should create a list of stars with values 5 to 1', (): void => {
+		expect(userRating.stars.length).to.equal(5);
+		expect(userRating.stars[0].value).to.equal(5);
+		expect(userRating.stars[1].value).to.equal(4);
+		expect(userRating.stars[2].value).to.equal(3);
+		expect(userRating.stars[3].value).to.equal(2);
+		expect(userRating.stars[4].value).to.equal(1);
+	});
+
+	it('should set all stars less than or equal to the rating to filled', (): void => {
+		userRating.setRating(3);
+
+		expect(scope.ngModel.$viewValue).to.equal(3);
+
+		expect(userRating.stars[0].filled).to.be.false;
+		expect(userRating.stars[1].filled).to.be.false;
+		expect(userRating.stars[2].filled).to.be.true;
+		expect(userRating.stars[3].filled).to.be.true;
+		expect(userRating.stars[4].filled).to.be.true;
+	});
+
+	it('should set the initial view state based on the initial view value', (): void => {
+		scope.ngModel.$viewValue = 2;
+		scope.$digest();
+
+		expect(userRating.stars[0].filled).to.be.false;
+		expect(userRating.stars[1].filled).to.be.false;
+		expect(userRating.stars[2].filled).to.be.false;
+		expect(userRating.stars[3].filled).to.be.true;
+		expect(userRating.stars[4].filled).to.be.true;
+	});
+
+	function buildController(): void {
+		var ngModel: INgModelMock = {
+			$setViewValue: (value: number): void => { ngModel.$viewValue = value; },
+			$viewValue: null,
+		};
+		var controllerResult: __angularFixture.IControllerResult<__userRating.IUserRatingController>
+			= __angularFixture.angularFixture.controller<__userRating.IUserRatingController>(__userRating.controllerName, { ngModel: ngModel });
+
+		scope = <__userRating.IUserRatingScope>controllerResult.scope;
+		userRating = controllerResult.controller;
+	}
+});
