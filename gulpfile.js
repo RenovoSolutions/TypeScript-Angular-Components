68f48783b3629de1e61555921af94693b81dbe38
Josh Graber <jgraber@renovo1.com>
2015-06-02 15:08:30 -0400

    Configure gulp tasks and set up karma.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/// <vs BeforeBuild='build' Clean='clean' />
+var gulp = require('gulp');
+var gulpUtilities = require('gulp-utilities');
+
+var browserify = gulpUtilities.browserify;
+var main = 'components'; 
+
+gulp.task('compile.debug', function(done) {
+	return browserify.compileDebug(main);
+});
+
+gulp.task('compile.release', function(done) {
+	return browserify.compileRelease(main);
+});
+
+gulp.task('compile', ['compile.debug']);
+
+gulpUtilities.build.config(gulp, 'compile.debug', 'compile.release');
+gulpUtilities.teamCity.config(gulp);
+gulpUtilities.test.config(gulp, __dirname + '/karma.conf.js');
+
+gulp.task('default', ['build']);
