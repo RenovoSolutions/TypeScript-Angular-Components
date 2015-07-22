f372baa901b1031511ba5f446fd494d3c1aa2921
Josh Graber <jgraber@renovo1.com>
2015-07-22 14:21:33 -0400

    Install utilities as a bower dependency.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,8 +1,7 @@
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="bootstrap/bootstrap.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
-
-/// <reference path="../libraries/utilities/utilities.d.ts" />
-
 /// <reference path="angularMocks.d.ts" />
 /// <reference path="requirejs/require.d.ts" />
+
+/// <reference path="../libraries/rl-utilities/typings/utility.d.ts" />
