288d1770af01831d6b8cf264448a535d4ebbd7ac
SamGraber <sgraber@renovo1.com>
2015-09-16 16:30:54 -0400

    Implemented dataSourceProcessor

--- source/components/cardContainer/dataSources/dataSources.module.ts
+++ source/components/cardContainer/dataSources/dataSources.module.ts
@@ -1,8 +1,12 @@
 // uses typings/angularjs
+// uses typescript-angular-utilities
 
 // /// <reference path='../../../typings/angularjs/angular.d.ts' />
+// /// <reference path='../../../libraries/typescript-angular-utilities/typings/utility.d.ts' />
 
+/// <reference path='../sorts/sorter/sorter.service.ts' />
 /// <reference path='dataPager/dataPager.service.ts' />
+/// <reference path='dataSourceProcessor.service.ts' />
 
 module rl.ui.components.cardContainer.dataSources {
 	'use strict';
@@ -10,6 +14,10 @@ module rl.ui.components.cardContainer.dataSources {
 	export var moduleName: string = 'rl.ui.components.cardContainer.dataSources';
 	
 	angular.module(moduleName, [
+		rl.utilities.services.object.moduleName,
+		sorts.sorter.moduleName,
+		
 		dataPager.moduleName,
-	]);
+	])
+		.service(processorServiceName, DataSourceProcessor);
 }
