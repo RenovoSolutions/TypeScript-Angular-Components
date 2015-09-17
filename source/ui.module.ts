6ae125fc768a8cb2fe6bb1825831ff2fd17d24fb
SamGraber <sgraber@renovo1.com>
2015-09-17 13:11:19 -0400

    Implemented autosave behavior. This is required for card.

--- source/ui.module.ts
+++ source/ui.module.ts
@@ -1,5 +1,6 @@
 // uses angularjs
 
+/// <reference path='behaviors/behaviors.module.ts' />
 /// <reference path='components/components.module.ts' />
 /// <reference path='services/services.module.ts' />
 
@@ -7,6 +8,7 @@ module rl.ui {
 	export var moduleName: string = 'rl.ui';
 
 	angular.module(name, [
+		behaviors.moduleName,
 		components.moduleName,
 		services.moduleName,
 	]);
