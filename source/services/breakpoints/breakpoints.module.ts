641c6dc7352641af0c0896dfe85ee75a58275e3e
SamGraber <sgraber@renovo1.com>
2015-09-17 08:33:46 -0400

    Added an 'enum' for breakpoints

---
+++ source/services/breakpoints/breakpoints.module.ts
@@ -0,0 +1,9 @@
+// /// <reference path='../../typings/angularjs/angular.d.ts' />
+
+module rl.ui.services.breakpoints {
+	export var moduleName: string = 'rl.ui.services.breakpoints';
+	
+	angular.module(moduleName, [
+		
+	]);
+}
