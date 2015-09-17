705f5b0e58ab5f6d50ed37ce4a9daeada912091a
SamGraber <sgraber@renovo1.com>
2015-09-17 08:27:33 -0400

    Implemented windowWrapper in components. This is required for breakpoints, which is used in the card container.

--- source/services/services.module.ts
+++ source/services/services.module.ts
@@ -1,9 +1,11 @@
 // /// <reference path='../../typings/angularjs/angular.d.ts' />
 
+/// <reference path='windowWrapper/windowWrapper.service.ts' />
+
 module rl.ui.services {
 	export var moduleName: string = 'rl.ui.services';
 	
 	angular.module(moduleName, [
-		
+		windowWrapper.moduleName,
 	]);
 }
