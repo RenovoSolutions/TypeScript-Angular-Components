27a7b234d496e02afd7a40c6bc7556b86d131d02
SamGraber <sgraber@renovo1.com>
2015-09-17 08:50:02 -0400

    Added a constant for the resizeDebounce

--- source/services/breakpoints/breakpoints.module.ts
+++ source/services/breakpoints/breakpoints.module.ts
@@ -1,6 +1,7 @@
 // /// <reference path='../../typings/angularjs/angular.d.ts' />
 // /// <reference path='../../../libraries/typescript-angular-utilities/typings/utility.d.ts' />
 
+/// <reference path='../windowWrapper/windowWrapper.service.ts' />
 /// <reference path='visibleBreakpoint.service.ts' />
 /// <reference path='breakpoints.service.ts' />
 
@@ -9,7 +10,9 @@ module rl.ui.services.breakpoints {
 	
 	angular.module(moduleName, [
 		rl.utilities.services.observable.moduleName,
+		windowWrapper.moduleName,
 	])
+		.constant('resizeDebounceMilliseconds', 500)
 		.service(visibleBreakpointServiceName, VisibleBreakpointService)
 		.service(breakpointServiceName, BreakpointService);
 }
