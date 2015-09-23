73b945b070ac3bde6ed3adfb47b7b3345026b1cc
SamGraber <sgraber@renovo1.com>
2015-09-23 10:25:54 -0400

    Updated autosaveDialog module

--- source/services/autosaveDialog/autosaveDialog.module.ts
+++ source/services/autosaveDialog/autosaveDialog.module.ts
@@ -1,14 +1,14 @@
-/// <reference path='../../../typings/angularjs/angular.d.ts' />
+// /// <reference path='../../../typings/angularjs/angular.d.ts' />
+
+/// <reference path='autosaveDialog.service.ts' />
+/// <reference path='autosaveDialog.controller.ts' />
 
 module rl.ui.services.autosaveDialog {
 	'use strict';
 
 	export var moduleName: string = 'rl.ui.services.autosaveDialog';
 
-	// import __autosaveDialog = require('./autosaveDialog.service');
-	// import __autosaveDialogController = require('./autosaveDialog.controller');
-
-	angular.module(moduleName, []);
-		// .service(__autosaveDialog.name, __autosaveDialog.AutosaveDialogService)
-		// .controller(__autosaveDialogController.controllerName, __autosaveDialogController.AutosaveDialogController);
+	angular.module(moduleName, [])
+		.service(serviceName, AutosaveDialogService)
+		.controller(controllerName, AutosaveDialogController);
 }
