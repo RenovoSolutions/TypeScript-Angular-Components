e3a3af3fa2ed3aaa176a6dc939f9e8678f9157bd
Josh Graber <jgraber@renovo1.com>
2015-09-25 10:23:56 -0400

    Update type definition versions. Remove angularMocks (replace by angular-mocks type definition). Fix type definition references in tsd.d.ts

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,13 +1,12 @@
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="bootstrap/bootstrap.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
-/// <reference path="angularMocks.d.ts" />
-/// <reference path="requirejs/require.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="sinon/sinon.d.ts" />
 /// <reference path="moment/moment-node.d.ts" />
 /// <reference path="moment/moment.d.ts" />
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
-
-/// <reference path="../libraries/typescript-angular-utilities/typings/utility.d.ts" />
 /// <reference path="bootstrap-touchspin/bootstrap-touchspin.d.ts" />
 /// <reference path="signature_pad/signature_pad.d.ts" />
 /// <reference path="angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
+/// <reference path="angularjs/angular-mocks.d.ts" />
