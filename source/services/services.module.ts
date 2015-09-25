9e83fbf9ece92cf166bbfa39e8d7f07929541fee
Josh Graber <jgraber@renovo1.com>
2015-09-25 14:51:49 -0400

    Port contentProvider over from old utilities and migrate to ES6.

--- source/services/services.module.ts
+++ source/services/services.module.ts
@@ -1,7 +1,10 @@
+'use strict';
+
 import * as angular from 'angular';
 
 import * as autosaveDialog from './autosaveDialog/autosaveDialog.module';
 import * as breakpoints from './breakpoints/breakpoints.module';
+import * as contentProvider from './contentProvider/contentProvider.service';
 import * as dialog from './dialog/dialog.service';
 import * as jquery from './jquery/jquery.service';
 import * as windowWrapper from './windowWrapper/windowWrapper.service';
@@ -9,6 +12,7 @@ import * as windowWrapper from './windowWrapper/windowWrapper.service';
 export {
 	autosaveDialog,
 	breakpoints,
+	contentProvider,
 	dialog,
 	jquery,
 	windowWrapper,
@@ -19,6 +23,7 @@ export var moduleName: string = 'rl.ui.services';
 angular.module(moduleName, [
 	autosaveDialog.moduleName,
 	breakpoints.moduleName,
+	contentProvider.moduleName,
 	dialog.moduleName,
 	jquery.moduleName,
 	windowWrapper.moduleName,
