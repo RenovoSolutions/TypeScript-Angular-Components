1b51868f4375c8d4fc204d38ccb88dcdcdac64e3
Josh Graber <jgraber@renovo1.com>
2015-09-25 10:27:36 -0400

    Migrate autosave to ES6 syntax

--- source/behaviors/autosave/autosave.ts
+++ source/behaviors/autosave/autosave.ts
@@ -1,92 +1,88 @@
-// uses typings/angularjs
-// uses typescript-angular-utilities
+'use strict';
 
-// /// <reference path='../../typings/angularjs/angular.d.ts' />
-// /// <reference path='../../../libraries/typescript-angular-utilities/typings/utility.d.ts' />
+import * as angular from 'angular';
 
-module rl.ui.behaviors.autosave {
-	'use strict';
-	
-	export var moduleName: string = 'rl.ui.behaviors.autosave';
-	export var directiveName: string = 'rlAutosave';
-	export var controllerName: string = 'AutosaveController';
-	
-	import __autosave = rl.utilities.services.autosave;
-	import __parentChild = rl.utilities.services.parentChildBehavior;
-	import __objectUtility = rl.utilities.services.object;
-	import __autosaveAction = rl.utilities.services.autosaveAction;
-	
-	export interface IAutosaveAttributes extends ng.IAttributes {
-		rlAutosave: string;
-		validate: string;
-		save: string;
-	}
-	
-	export interface IAutosaveBehavior {
-		autosave(): boolean;
-	}
-	
-	export class AutosaveController {
-		static $inject: string[] = ['$scope'
-								, '$attrs'
-								, '$parse'
-								, '$element'
-								, __autosave.factoryName
-								, __parentChild.serviceName
-								, __objectUtility.serviceName
-								, __autosaveAction.serviceName];
-		constructor(private $scope: ng.IScope
-			, $attrs: IAutosaveAttributes
-			, $parse: ng.IParseService
-			, $element: ng.IAugmentedJQuery
-			, autosaveFactory: __autosave.IAutosaveServiceFactory
-			, parentChildBehavior: __parentChild.IParentChildBehaviorService
-			, objectUtility: __objectUtility.IObjectUtility) {
-			var contentForm: ng.IFormController = $element.controller('form');
-	
-			var hasValidator: boolean = objectUtility.isNullOrWhitespace($attrs.validate) === false;
-	
-			var validateExpression: ng.ICompiledExpression = $parse($attrs.validate);
-			var validate: { (): boolean };
-	
-			if (hasValidator) {
-				validate = (): boolean => {
-					return validateExpression($scope);
-				};
-			}
-	
-			var saveExpression: ng.ICompiledExpression = $parse($attrs.save);
-			var save: { (): ng.IPromise<void> } = (): ng.IPromise<void> => {
-				return saveExpression($scope);
-			};
-	
-			var autosave: __autosave.IAutosaveService = autosaveFactory.getInstance(save, contentForm, validate);
-	
-			var behavior: IAutosaveBehavior = {
-				autosave: autosave.autosave,
-			};
-	
-			// register autosave behavior and assign the value back to the parent
-			var childLink: any = $parse($attrs.rlAutosave)($scope);
-			parentChildBehavior.registerChildBehavior(childLink, behavior);
-		}
-	}
-	
-	export function autosave(): ng.IDirective {
-		'use strict';
-		return {
-			restrict: 'A',
-			require: '?ngForm',
-			controller: controllerName,
-		};
-	}
-	
-	angular.module(moduleName, [
-		__autosave.moduleName,
-		__autosaveAction.moduleName,
-		__objectUtility.moduleName,
-		__parentChild.moduleName,	
-	])
-		.directive(directiveName, autosave)
-		.controller(controllerName, AutosaveController);
+import { services } from 'typescript-angular-utilities';
+
+export var moduleName: string = 'rl.ui.behaviors.autosave';
+export var directiveName: string = 'rlAutosave';
+export var controllerName: string = 'AutosaveController';
+
+import __autosave = services.autosave;
+import __parentChild = services.parentChildBehavior;
+import __objectUtility = services.object;
+import __autosaveAction = services.autosaveAction;
+
+export interface IAutosaveAttributes extends angular.IAttributes {
+	rlAutosave: string;
+	validate: string;
+	save: string;
 }
+
+export interface IAutosaveBehavior {
+	autosave(): boolean;
+}
+
+export class AutosaveController {
+	static $inject: string[] = ['$scope'
+							, '$attrs'
+							, '$parse'
+							, '$element'
+							, __autosave.factoryName
+							, __parentChild.serviceName
+							, __objectUtility.serviceName
+							, __autosaveAction.serviceName];
+	constructor(private $scope: angular.IScope
+		, $attrs: IAutosaveAttributes
+		, $parse: angular.IParseService
+		, $element: angular.IAugmentedJQuery
+		, autosaveFactory: __autosave.IAutosaveServiceFactory
+		, parentChildBehavior: __parentChild.IParentChildBehaviorService
+		, objectUtility: __objectUtility.IObjectUtility) {
+		var contentForm: angular.IFormController = $element.controller('form');
+
+		var hasValidator: boolean = objectUtility.isNullOrWhitespace($attrs.validate) === false;
+
+		var validateExpression: angular.ICompiledExpression = $parse($attrs.validate);
+		var validate: { (): boolean };
+
+		if (hasValidator) {
+			validate = (): boolean => {
+				return validateExpression($scope);
+			};
+		}
+
+		var saveExpression: angular.ICompiledExpression = $parse($attrs.save);
+		var save: { (): angular.IPromise<void> } = (): angular.IPromise<void> => {
+			return saveExpression($scope);
+		};
+
+		var autosave: __autosave.IAutosaveService = autosaveFactory.getInstance(save, contentForm, validate);
+
+		var behavior: IAutosaveBehavior = {
+			autosave: autosave.autosave,
+		};
+
+		// register autosave behavior and assign the value back to the parent
+		var childLink: any = $parse($attrs.rlAutosave)($scope);
+		parentChildBehavior.registerChildBehavior(childLink, behavior);
+	}
+}
+
+export function autosave(): angular.IDirective {
+	'use strict';
+	return {
+		restrict: 'A',
+		require: '?ngForm',
+		controller: controllerName,
+	};
+}
+
+angular.module(moduleName, [
+	__autosave.moduleName,
+	__autosaveAction.moduleName,
+	__objectUtility.moduleName,
+	__parentChild.moduleName,
+])
+	.directive(directiveName, autosave)
+	.controller(controllerName, AutosaveController);
--- source/behaviors/behaviors.module.ts
+++ source/behaviors/behaviors.module.ts
@@ -1,8 +1,11 @@
 import * as angular from 'angular';
 
+import * as autosave from './autosave/autosave';
 
+export { autosave };
 
 export var moduleName: string = 'rl.ui.behaviors';
 
 angular.module(moduleName, [
+	autosave.moduleName,
 ]);
