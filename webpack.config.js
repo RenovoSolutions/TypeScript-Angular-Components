dd93c00b54dacc468fa810a342e7030b492a1396
Josh Graber <jgraber@renovo1.com>
2015-09-30 14:01:21 -0400

    Add a css loader to the webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@ var webpack = require('gulp-utilities').webpack;
 var library = require('./webpack.library.json');
 
 var webpackRawLoader = require('./webpack.raw-loader');
+var webpackCssLoader = require('./webpack.css-loader');
 
 var webpackConfig = webpack.library(library);
 webpackRawLoader(webpackConfig);
+webpackCssLoader(webpackConfig);
 
 module.exports = webpackConfig;
