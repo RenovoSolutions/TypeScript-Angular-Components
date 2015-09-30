a0d2d4f74374113b1f54540d6f0f76d130ce07dc
Josh Graber <jgraber@renovo1.com>
2015-09-30 14:02:23 -0400

    All of the component dependencies should be loaded here. They shouldn't need to be loaded by the consumer of components.

--- source/ui.module.ts
+++ source/ui.module.ts
@@ -2,6 +2,15 @@
 
 import * as angular from 'angular';
 
+import 'angular-ui-bootstrap';
+import 'angular-sanitize';
+import 'rangy';
+import 'textangular';
+
+import '../libraries/angular-bootstrap-slider/index';
+
+import 'signature_pad';
+
 import * as behaviors from './behaviors/behaviors.module';
 import * as components from './components/components.module';
 import * as services from './services/services.module';
@@ -11,6 +20,10 @@ export { behaviors, components, services };
 export var moduleName: string = 'rl.ui';
 
 angular.module(moduleName, [
+	'ui.bootstrap',
+	'ngSanitize',
+	'textAngular',
+
 	behaviors.moduleName,
 	components.moduleName,
 	services.moduleName,
