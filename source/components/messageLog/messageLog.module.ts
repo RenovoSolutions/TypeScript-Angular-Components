bfac0e1bcb2ca8a2937edac9ae96ebfbca59802c
SamGraber <sgraber@renovo1.com>
2015-10-06 16:51:11 -0400

    Editable message log needs to be in the base message log module. It would be nice to have it in a separate module, but since it requires the message log service, we can't have it as a child module of the message log.

--- source/components/messageLog/messageLog.module.ts
+++ source/components/messageLog/messageLog.module.ts
@@ -4,6 +4,12 @@ import * as angular from 'angular';
 
 import { factoryName, messageLogFactory } from './messageLog.service';
 import { controllerName, directiveName, messageLog, MessageLogController } from './messageLog.directive';
+import {
+	editableMessageLog,
+	EditableMessageLogController,
+	directiveName as editableMessageLogDirectieName,
+	controllerName as editableMessageLogControllerName
+} from './editableMessageLog';
 
 export * from './messageLog.service';
 export * from './messageLog.directive';
@@ -13,4 +19,6 @@ export var moduleName: string = 'rl.ui.components.messageLog';
 angular.module(moduleName, [])
 	.factory(factoryName, messageLogFactory)
 	.directive(directiveName, messageLog)
-	.controller(controllerName, MessageLogController);
+	.controller(controllerName, MessageLogController)
+	.directive(editableMessageLogDirectieName, editableMessageLog)
+	.controller(editableMessageLogControllerName, EditableMessageLogController);
