9e53d517129ce57dc0387f10ce178f6b2a93114b
SamGraber <sgraber@renovo1.com>
2015-10-23 12:51:00 -0400

    If the signature pad is disabled, render the signature with an image tag.

--- source/components/signaturePad/signaturePad.ts
+++ source/components/signaturePad/signaturePad.ts
@@ -12,6 +12,9 @@ export interface ISignaturePadScope extends angular.IScope {
 	pad: SignaturePad;
 	height: number;
 	width: number;
+	ngDisabled: boolean;
+
+	style: any;
 }
 
 export function signaturePad(): angular.IDirective {
@@ -19,28 +22,39 @@ export function signaturePad(): angular.IDirective {
 	return {
 		restrict: 'E',
 		template: `
-			<canvas class="signature-pad"></canvas>
+			<canvas class="signature-pad" ng-if="!ngDisabled"></canvas>
+			<img ng-src="{{signature}}" ng-style="style" ng-if="ngDisabled" />
 		`,
 		scope: {
 			signature: '=',
 			pad: '=',
 			height: '=',
 			width: '=',
+			ngDisabled: '=',
 		},
 		link(scope: ISignaturePadScope, element: angular.IAugmentedJQuery): void {
-			var canvas: HTMLCanvasElement = <HTMLCanvasElement>element.find('.signature-pad').get(0);
-			var options: any = {
-				backgroundColor: 'rgb(255, 255, 255)',
-			};
+			scope.$watch('ngDisabled', (disabled: boolean): void => {
+				if (disabled) {
+					scope.style = {
+						height: scope.height != null ? scope.height : 100,
+						width: scope.width != null ? scope.width : 200,
+					};
+				} else {
+					var canvas: HTMLCanvasElement = <HTMLCanvasElement>element.find('.signature-pad').get(0);
+					var options: any = {
+						backgroundColor: 'rgb(255, 255, 255)',
+					};
 
-			scope.pad = new SignaturePad(canvas, options);
+					scope.pad = new SignaturePad(canvas, options);
 
-			canvas.height = scope.height != null ? scope.height : 100;
-			canvas.width = scope.width != null ? scope.width : 200;
+					canvas.height = scope.height != null ? scope.height : 100;
+					canvas.width = scope.width != null ? scope.width : 200;
 
-			scope.$watch('signature', (value: string): void => {
-				if (value != null) {
-					scope.pad.fromDataURL(value);
+					scope.$watch('signature', (value: string): void => {
+						if (value != null) {
+							scope.pad.fromDataURL(value);
+						}
+					});
 				}
 			});
 		},
