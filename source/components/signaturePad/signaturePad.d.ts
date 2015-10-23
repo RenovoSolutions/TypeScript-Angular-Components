223431ba7abdc581fadad9052f9b6b6742981e1a
Josh Graber <jgraber@renovo1.com>
2015-10-23 15:34:11 -0400

    Build

--- source/components/signaturePad/signaturePad.d.ts
+++ source/components/signaturePad/signaturePad.d.ts
@@ -1,10 +1,13 @@
 import * as angular from 'angular';
+import * as SignaturePad from 'signature_pad';
 export declare var moduleName: string;
 export declare var directiveName: string;
 export interface ISignaturePadScope extends angular.IScope {
-    signature: SignaturePad;
-    initial: string;
+    pad: SignaturePad;
     height: number;
     width: number;
+    ngDisabled: boolean;
+    style: any;
+    ngModel: angular.INgModelController;
 }
 export declare function signaturePad(): angular.IDirective;
--- source/components/signaturePad/signaturePad.js
+++ source/components/signaturePad/signaturePad.js
@@ -1,30 +1,49 @@
-// /// <reference path='../../../typings/signature_pad/signature_pad.d.ts' />
 'use strict';
 var angular = require('angular');
+var SignaturePad = require('signature_pad');
 exports.moduleName = 'rl.ui.components.signaturePad';
 exports.directiveName = 'rlSignaturePad';
 function signaturePad() {
     'use strict';
     return {
         restrict: 'E',
-        template: "\n\t\t\t<canvas class=\"signature-pad\"></canvas>\n\t\t",
+        require: 'ngModel',
+        template: "\n\t\t\t<canvas class=\"signature-pad\" ng-if=\"!ngDisabled\"></canvas>\n\t\t\t<img ng-src=\"{{ngModel.$viewValue}}\" ng-style=\"style\" ng-if=\"ngDisabled\" />\n\t\t",
         scope: {
-            signature: '=',
-            initial: '=',
+            pad: '=',
             height: '=',
             width: '=',
+            ngDisabled: '=',
         },
-        link: function (scope, element) {
-            var canvas = element.find('.signature-pad').get(0);
-            var options = {
-                backgroundColor: 'rgb(255, 255, 255)',
-            };
-            scope.signature = new SignaturePad(canvas, options);
-            canvas.height = scope.height != null ? scope.height : 100;
-            canvas.width = scope.width != null ? scope.width : 200;
-            if (scope.initial != null) {
-                scope.signature.fromDataURL(scope.initial);
-            }
+        link: function (scope, element, attrs, ngModel) {
+            scope.$watch('ngDisabled', function (disabled) {
+                scope.ngModel = ngModel;
+                if (disabled) {
+                    scope.style = {
+                        height: scope.height != null ? scope.height : 100,
+                        width: scope.width != null ? scope.width : 200,
+                    };
+                }
+                else {
+                    var canvas = element.find('.signature-pad').get(0);
+                    var options = {
+                        backgroundColor: 'rgb(255, 255, 255)',
+                    };
+                    scope.pad = new SignaturePad(canvas, options);
+                    canvas.height = scope.height != null ? scope.height : 100;
+                    canvas.width = scope.width != null ? scope.width : 200;
+                    scope.$watch(function () { return ngModel.$viewValue; }, function (value) {
+                        if (value != null) {
+                            scope.pad.fromDataURL(value);
+                        }
+                    });
+                    scope.$watch(function () { return scope.pad.toDataURL(); }, function (value) {
+                        if (value != null) {
+                            ngModel.$setViewValue(value);
+                        }
+                    });
+                }
+            });
         },
     };
 }
