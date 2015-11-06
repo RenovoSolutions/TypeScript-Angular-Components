511a1389679223f77d3ab7a8d8d6e7c2b38269a7
Josh Graber <jgraber@renovo1.com>
2015-11-06 16:19:03 -0500

    Allow templates to be specified for displaying messages differently based on a selector.
    Conflicts:
    	source/components/messageLog/messageLog.html

--- source/components/messageLog/messageLog.directive.ts
+++ source/components/messageLog/messageLog.directive.ts
@@ -5,8 +5,17 @@
 import * as ng from 'angular';
 
 import { services } from 'typescript-angular-utilities';
+import __object = services.object;
+
+import {
+	moduleName as jqueryModuleName,
+	serviceName as jqueryServiceName,
+	IJQueryUtility,
+} from '../../services/jquery/jquery.service';
 
 import { IMessageLogDataService, IMessageLog, IMessage, factoryName, IMessageLogFactory } from './messageLog.service';
+
+import { IGenericTemplate } from '../genericContainer/genericContainer';
 
 export var directiveName: string = 'rlMessageLog';
 export var controllerName: string = 'MessageLogController';
@@ -15,18 +24,23 @@ export interface IMessageLogBindings {
 	pageSize: number;
 	service: IMessageLogDataService;
 	messageLogBinding: IMessageLog;
+
+	selector: { (IMessage): any } | string;
 }
 
-export class MessageLogController {
+export class MessageLogController implements IMessageLogBindings {
 	// bindings
 	pageSize: number;
 	service: IMessageLogDataService;
 	messageLogBinding: IMessageLog;
+	selector: { (IMessage): any } | string;
 
 	messages: IMessage[];
 	hasNextPage: boolean;
 	hasPreviousPage: boolean;
 	messageLog: IMessageLog;
+
+	templates: any;
 
 	loading: boolean;
 	loadingInitial: boolean;
@@ -65,6 +79,14 @@ export class MessageLogController {
 		this.messageLog.pageSize = this.pageSize != null ? this.pageSize : 8;
 	}
 
+	getEntrySelector(entry: IMessage): any {
+		if (_.isString(this.selector)) {
+			return entry[<string> this.selector];
+		} else if (_.isFunction(this.selector)) {
+			return (<{ (IMessage): any }> this.selector)(entry);
+		}
+	}
+
 	getOlder(): ng.IPromise<void> {
 		return this.messageLog.getNextPage();
 	}
@@ -74,18 +96,53 @@ export class MessageLogController {
 	}
 }
 
-export function messageLog(): ng.IDirective {
+interface IMessageLogScope {
+	defaultTemplate: IGenericTemplate | string;
+
+}
+
+messageLog.$inject = ['$interpolate', jqueryServiceName, __object.serviceName];
+export function messageLog($interpolate: angular.IInterpolateService,
+							jquery: IJQueryUtility,
+							object: __object.IObjectUtility): angular.IDirective {
 	'use strict';
 	return {
 		restrict: 'E',
 		template: require('./messageLog.html'),
+		transclude: true,
 		controller: controllerName,
 		controllerAs: 'log',
 		scope: {},
 		bindToController: {
 			service: '=',
+			selector: '=',
 			pageSize: '=',
 			messageLogBinding: '=messageLog',
 		},
+		link: (scope: angular.IScope,
+			   element: angular.IAugmentedJQuery,
+			   attributes: angular.IAttributes,
+			   controller: MessageLogController,
+			   transclude: angular.ITranscludeFunction): void => {
+			controller.templates = {};
+
+			// Load templates from the DOM
+			transclude((clone: angular.IAugmentedJQuery,
+						transclusionScope: angular.IScope): void => {
+				var templates: JQuery = clone.filter('template');
+
+				templates.each((index: number,
+								template: Element): void => {
+					var templateElement: angular.IAugmentedJQuery = angular.element(template);
+					var templateHtml: string = templateElement.html();
+
+					var triggerAttribute: string = templateElement.attr('when-selector');
+					if (!object.isNullOrWhitespace(triggerAttribute)) {
+						var trigger: string = $interpolate(triggerAttribute)(transclusionScope);
+						controller.templates[trigger] = templateHtml;
+					}
+				});
+			});
+		}
 	};
 }
