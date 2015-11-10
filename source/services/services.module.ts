e153851f556281e8d13c3d59c8c521068294a1e6
SamGraber <sgraber@renovo1.com>
2015-11-10 14:55:27 -0500

    Implemented componentValidator in components.

--- source/services/services.module.ts
+++ source/services/services.module.ts
@@ -4,6 +4,7 @@ import * as angular from 'angular';
 
 import * as autosaveDialog from './autosaveDialog/autosaveDialog.module';
 import * as breakpoints from './breakpoints/breakpoints.module';
+import * as componentValidator from './componentValidator/componentValidator.service';
 import * as contentProvider from './contentProvider/contentProvider.service';
 import * as dialog from './dialog/dialog.service';
 import * as jquery from './jquery/jquery.service';
@@ -12,6 +13,7 @@ import * as windowWrapper from './windowWrapper/windowWrapper.service';
 export {
 	autosaveDialog,
 	breakpoints,
+	componentValidator,
 	contentProvider,
 	dialog,
 	jquery,
@@ -23,6 +25,7 @@ export var moduleName: string = 'rl.ui.services';
 angular.module(moduleName, [
 	autosaveDialog.moduleName,
 	breakpoints.moduleName,
+	componentValidator.moduleName,
 	contentProvider.moduleName,
 	dialog.moduleName,
 	jquery.moduleName,
