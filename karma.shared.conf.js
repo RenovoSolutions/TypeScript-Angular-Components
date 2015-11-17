62050b5b41446a137906141832f8be2d2427d234
Josh Graber <jgraber@renovo1.com>
2015-11-17 16:05:57 -0500

    Include jquery and angular as external test dependencies and combine test setup code into one file.

---
+++ karma.shared.conf.js
@@ -0,0 +1,19 @@
+// Karma default configuration
+
+var webpackRawLoader = require('./webpack.raw-loader');
+
+module.exports = function (karma, karmaSettings) {
+	var karmaConfig = karmaSettings(karma, [
+		'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js',
+		'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular.min.js',
+		'test-bootstrapper.js',
+	]);
+	webpackRawLoader(karmaConfig.webpack);
+	karmaConfig.webpack.externals = {
+		'jquery': '$',
+		'angular': 'angular',
+	};
+
+	karma.set(karmaConfig);
+	return karmaConfig;
+};
