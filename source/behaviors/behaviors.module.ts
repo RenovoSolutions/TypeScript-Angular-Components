13598a2fa2c06cabcc54ac12cf0766aed82a2a34
SamGraber <samgraber@sundialproperties.com>
2015-11-19 11:26:37 -0500

    Created a simple component for aliasing data objects using `item as name` syntax.

--- source/behaviors/behaviors.module.ts
+++ source/behaviors/behaviors.module.ts
@@ -1,11 +1,13 @@
 import * as angular from 'angular';
 
+import * as alias from './alias/alias';
 import * as autosave from './autosave/autosave';
 
-export { autosave };
+export { alias, autosave };
 
 export var moduleName: string = 'rl.ui.behaviors';
 
 angular.module(moduleName, [
+	alias.moduleName,
 	autosave.moduleName,
 ]);
