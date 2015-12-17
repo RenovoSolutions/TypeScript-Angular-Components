bdf717b5855c75cd6623f3efe58cd1188b4abf52
TheOriginalJosh <theoriginaljosh@gmail.com>
2015-12-17 14:50:56 -0500

    Select filter factory and tests.

--- source/components/cardContainer/filters/selectFilter/selectFilter.service.ts
+++ source/components/cardContainer/filters/selectFilter/selectFilter.service.ts
@@ -1,23 +1,40 @@
 'use strict';
 
+import * as _ from 'lodash';
+import {filters} from 'typescript-angular-utilities';
+
 export let factoryName: string = 'selectFilter';
 
-export interface ISelectFilter {
+export interface ISelectFilter<T> extends filters.IFilter {
+	selectedValue: any;
+}
+
+class SelectFilter<T> implements ISelectFilter<T> {
+	selectedValue: any;
+	type: string = 'selectFilter';
+
+	constructor(private valueSelector: string | { (item:T):any }) {}
+
+	filter(item: T): boolean {
+		return this.getValue(item) === this.selectedValue;
+	}
+
+	private getValue(item: T):any {
+		return (_.isFunction(this.valueSelector)
+			? (<{ (item: T): any }>this.valueSelector)(item)
+			: item[<string>this.valueSelector]);
+	}
 
 }
 
-class SelectFilter {
-
-}
-
-export interface ISelectFilterFactory {
-	getInstance(): ISelectFilter;
+export interface ISelectFilterFactory  {
+	getInstance<T>(valueSelector: string | { (item:T):any }): ISelectFilter<T>;
 }
 
 export function selectFilterFactory(): ISelectFilterFactory {
 	return {
-		getInstance(): ISelectFilter {
-			return SelectFilter;
+		getInstance<T>(valueSelector: string | { (item:T):any }): ISelectFilter<T> {
+			return new SelectFilter<T>(valueSelector);
 		},
 	};
 }
