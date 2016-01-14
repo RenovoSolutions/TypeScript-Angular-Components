3e3e2c48cb14a9f7eee24b3db7a6df6add2f86cd
Josh Graber <jgraber@renovo1.com>
2016-01-14 12:18:08 -0500

    Only show the default styling for the busy spinner if using the default theme.

--- source/components/busy/busy.ts
+++ source/components/busy/busy.ts
@@ -4,19 +4,29 @@ import './busy.css';
 
 import * as angular from 'angular';
 
+import { defaultThemeValueName } from '../componentsDefaultTheme';
+
 export var moduleName: string = 'rl.ui.components.busy';
 export var directiveName: string = 'rlBusy';
 
-function busy(): angular.IDirective {
+interface IBusyScope extends angular.IScope {
+	useDefaultTheme: boolean;
+}
+
+busy.$inject = [defaultThemeValueName];
+function busy(useDefaultTheme: boolean): angular.IDirective {
 	'use strict';
 	return {
 		restrict: 'E',
-		template: '<i class="busy rl-{{size}}" ng-show="loading"></i>',
+		template: `<i class="busy rl-{{size}}" ng-class="{ 'default-theme': useDefaultTheme }" ng-show="loading"></i>`,
 		scope: {
 			loading: '=',
 			// Valid values are:
 			// `lg`, `2x`, `3x`, `4x`, and `5x`
 			size: '@',
+		},
+		link: (scope: IBusyScope) => {
+			scope.useDefaultTheme = useDefaultTheme;
 		},
 	};
 }
