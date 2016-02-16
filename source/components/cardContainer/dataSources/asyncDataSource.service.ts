13c1873d12d16a92ba991f9db3060f55924b6f06
SamGraber <samgraber@sundialproperties.com>
2016-02-16 15:10:50 -0500

    Created a base implementation of asynchronous data sources. We can use this to share the logic for reloading data in a data source, which is currently duplicated across all async data sources.

---
+++ source/components/cardContainer/dataSources/asyncDataSource.service.ts
@@ -0,0 +1,61 @@
+'use strict';
+
+import * as angular from 'angular';
+
+import { services, filters } from 'typescript-angular-utilities';
+import __observable = services.observable;
+import __array = services.array;
+import __synchronizedRequests = services.synchronizedRequests;
+
+import { IDataSource } from './dataSource';
+import { DataSourceBase } from './dataSourceBase.service';
+import { IDataSourceProcessor } from './dataSourceProcessor.service';
+import * as events from './dataSourceEvents';
+
+export interface IDataSetFunction<TDataType> {
+	(params: any): angular.IPromise<TDataType[]>;
+}
+
+export interface IAsyncDataSource<TDataType> extends IDataSource<TDataType> {
+	reload();
+	getDataSet: IDataSetFunction<TDataType>;
+}
+
+export class AsyncDataSource<TDataType> extends DataSourceBase<TDataType> implements IAsyncDataSource<TDataType> {
+	protected synchronizedRequests: __synchronizedRequests.ISynchronizedRequestsService;
+
+	constructor(getDataSet: IDataSetFunction<TDataType>
+			, observableFactory: __observable.IObservableServiceFactory
+			, dataSourceProcessor: IDataSourceProcessor
+			, array: __array.IArrayUtility
+			, synchronizedRequestsFactory: __synchronizedRequests.ISynchronizedRequestsFactory) {
+		super(observableFactory, dataSourceProcessor, array);
+		this.synchronizedRequests = synchronizedRequestsFactory.getInstance(getDataSet, this.resolveReload.bind(this));
+	}
+
+	set getDataSet(value: IDataSetFunction<TDataType>) {
+		this.synchronizedRequests.dataProvider = value;
+	}
+
+	reload(): void {
+		this.dataSet = null;
+		this.rawDataSet = null;
+		this.loadingDataSet = true;
+
+		this.synchronizedRequests.getData(this.getParams());
+	}
+
+	private resolveReload: { (data: TDataType[]): void } = (data: TDataType[]): void => {
+		this.loadingDataSet = false;
+		this.rawDataSet = data;
+
+		this.refresh();
+		this.observable.fire(events.async.reloaded);
+		this.observable.fire(events.changed);
+	}
+
+	// override with params for getDataSet
+	protected getParams(): any {
+		return null;
+	}
+}
