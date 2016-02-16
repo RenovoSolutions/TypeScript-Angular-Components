9fa341f0eb3a636784537c749e69cee02047eaa8
SamGraber <samgraber@sundialproperties.com>
2016-02-16 16:24:53 -0500

    Implemented server-side data source.

--- source/components/cardContainer/dataSources/serverSideDataSource/serverSideDataSource.service.ts
+++ source/components/cardContainer/dataSources/serverSideDataSource/serverSideDataSource.service.ts
@@ -1,16 +1,17 @@
 'use strict';
 
 import * as angular from 'angular';
+import * as _ from 'lodash';
 
 import { services, filters } from 'typescript-angular-utilities';
 import __observable = services.observable;
 import __array = services.array;
 import __object = services.object;
-import __genericSearchFilter = services.genericSearchFilter;
 import __synchronizedRequests = services.synchronizedRequests;
 
 import { IAsyncDataSource, AsyncDataSource, IDataSetFunction } from '../asyncDataSource.service';
 import { IDataSourceProcessor, processorServiceName } from '../dataSourceProcessor.service';
+import { ISort, SortDirection } from '../../sorts/sort';
 
 export var moduleName: string = 'rl.ui.components.cardContainer.dataSources.serverSideDataSource';
 export var factoryName: string = 'serverSideDataSource';
@@ -20,7 +21,28 @@ export interface IServerSideDataSource<TDataType> extends IAsyncDataSource<TData
 }
 
 export interface IServerSearchFunction<TDataType> {
-	(searchParams: any): angular.IPromise<TDataType[]>;
+	(searchParams: any): angular.IPromise<IDataResult<TDataType>>;
+}
+
+export interface IServerSearchParams {
+	filters: {[index: string]: any};
+	sorts: ISortParams[];
+	paging: IPagingParams;
+}
+
+export interface ISortParams {
+	column: string;
+	direction: string;
+}
+
+export interface IPagingParams {
+	pageNumber: number;
+	pageSize: number;
+}
+
+export interface IDataResult<TDataType> {
+	dataSet: TDataType[];
+	count: number;
 }
 
 export class ServerSideDataSource<TDataType> extends AsyncDataSource<TDataType> {
@@ -30,11 +52,38 @@ export class ServerSideDataSource<TDataType> extends AsyncDataSource<TDataType> 
 			, array: __array.IArrayUtility
 			, private object: __object.IObjectUtility
 			, synchronizedRequestsFactory: __synchronizedRequests.ISynchronizedRequestsFactory) {
-		super(getDataSet, observableFactory, dataSourceProcessor, array, synchronizedRequestsFactory);
+		super(<any>getDataSet, observableFactory, dataSourceProcessor, array, synchronizedRequestsFactory);
 	}
 
 	refresh(): void {
 		this.reload();
+	}
+
+	protected getParams(): IServerSearchParams {
+		return {
+			filters: _.map(this.filters, (filter: filters.ISerializableFilter): any => {
+				if (_.isFunction(filter.serialize)) {
+					return filter.serialize();
+				}
+				return null;
+			}),
+			sorts: _.map(this.sorts, (sort: ISort): ISortParams => {
+				return {
+					column: sort.column.label,
+					direction: SortDirection.getFullName(sort.direction),
+				};
+			}),
+			paging: {
+				pageNumber: this.pager.pageNumber,
+				pageSize: this.pager.pageSize,
+			},
+		};
+	}
+
+	protected resolveReload(result: any): void {
+		let data: IDataResult<TDataType> = <IDataResult<TDataType>>result;
+		this.count = data.count;
+		super.resolveReload(data.dataSet);
 	}
 }
 
