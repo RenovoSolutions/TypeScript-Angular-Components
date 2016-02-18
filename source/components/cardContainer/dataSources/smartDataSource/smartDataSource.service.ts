c2ef3937623ffadfc74846c48b5e8c30ccfcac8f
SamGraber <samgraber@sundialproperties.com>
2016-02-18 14:08:42 -0500

    Shell implementation of a smart data source. Mostly duplicates server-side data source but without overriding refresh.

---
+++ source/components/cardContainer/dataSources/smartDataSource/smartDataSource.service.ts
@@ -0,0 +1,107 @@
+'use strict';
+
+import * as angular from 'angular';
+import * as _ from 'lodash';
+
+import { services, filters } from 'typescript-angular-utilities';
+import __observable = services.observable;
+import __array = services.array;
+import __object = services.object;
+import __synchronizedRequests = services.synchronizedRequests;
+
+import { IAsyncDataSource, AsyncDataSource, IDataSetFunction } from '../asyncDataSource.service';
+import { IDataSourceProcessor, processorServiceName } from '../dataSourceProcessor.service';
+import { ISort, SortDirection } from '../../sorts/sort';
+
+export var moduleName: string = 'rl.ui.components.cardContainer.dataSources.serverSideDataSource';
+export var factoryName: string = 'serverSideDataSource';
+
+export interface ISmartDataSource<TDataType> extends IAsyncDataSource<TDataType> {
+	filters: { [index: string]: filters.ISerializableFilter };
+}
+
+export interface IServerSearchFunction<TDataType> {
+	(searchParams: IServerSearchParams): angular.IPromise<IDataResult<TDataType>>;
+}
+
+export interface IServerSearchParams {
+	filters: {[index: string]: any};
+	sorts: ISortParams[];
+	paging: IPagingParams;
+}
+
+export interface ISortParams {
+	column: string;
+	direction: string;
+}
+
+export interface IPagingParams {
+	pageNumber: number;
+	pageSize: number;
+}
+
+export interface IDataResult<TDataType> {
+	dataSet: TDataType[];
+	count: number;
+}
+
+export class SmartDataSource<TDataType> extends AsyncDataSource<TDataType> {
+	throttled: boolean;
+
+	constructor(getDataSet: IServerSearchFunction<TDataType>
+			, observableFactory: __observable.IObservableServiceFactory
+			, dataSourceProcessor: IDataSourceProcessor
+			, array: __array.IArrayUtility
+			, private object: __object.IObjectUtility
+			, synchronizedRequestsFactory: __synchronizedRequests.ISynchronizedRequestsFactory) {
+		super(<any>getDataSet, observableFactory, dataSourceProcessor, array, synchronizedRequestsFactory);
+	}
+
+	protected getParams(): IServerSearchParams {
+		return {
+			filters: _.mapValues(this.filters, (filter: filters.ISerializableFilter): any => {
+				if (_.isFunction(filter.serialize)) {
+					return filter.serialize();
+				}
+				return null;
+			}),
+			sorts: _.map(this.sorts, (sort: ISort): ISortParams => {
+				return {
+					column: sort.column.label,
+					direction: SortDirection.getFullName(sort.direction),
+				};
+			}),
+			paging: {
+				pageNumber: this.pager.pageNumber,
+				pageSize: this.pager.pageSize,
+			},
+		};
+	}
+
+	protected resolveReload(result: any): void {
+		let data: IDataResult<TDataType> = <IDataResult<TDataType>>result;
+		this.count = data.count;
+		super.resolveReload(data.dataSet);
+	}
+}
+
+export interface ISmartDataSourceFactory {
+	getInstance<TDataType>(getDataSet: IServerSearchFunction<TDataType>): IAsyncDataSource<TDataType>;
+}
+
+smartDataSourceFactory.$inject = [__observable.factoryName, processorServiceName, __array.serviceName, __object.serviceName,  __synchronizedRequests.factoryName];
+export function smartDataSourceFactory(observableFactory: __observable.IObservableServiceFactory
+												, dataSourceProcessor: IDataSourceProcessor
+												, array: __array.IArrayUtility
+												, object: __object.IObjectUtility
+												, synchronizedRequestsFactory: __synchronizedRequests.ISynchronizedRequestsFactory): ISmartDataSourceFactory {
+	'use strict';
+	return {
+		getInstance<TDataType>(getDataSet: IServerSearchFunction<TDataType>): IAsyncDataSource<TDataType> {
+			return new SmartDataSource<TDataType>(getDataSet, observableFactory, dataSourceProcessor, array, object, synchronizedRequestsFactory);
+		},
+	};
+}
+
+angular.module(moduleName, [])
+	.factory(factoryName, smartDataSourceFactory);
