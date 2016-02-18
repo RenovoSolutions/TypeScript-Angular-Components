a39ebf4ccc0cf5535ae1b70ceab25e309a61d99e
SamGraber <samgraber@sundialproperties.com>
2016-02-18 13:26:50 -0500

    Custom typings for rx

---
+++ typings/rx.d.ts
@@ -0,0 +1,15 @@
+
+declare module "rx" {
+	export = Rx;
+}
+
+declare module Rx {
+	class Subject {
+		onNext(params: any): void;
+		subscribe(...params: any[]): Subscriber;
+	}
+
+	interface Subscriber {
+		dispose(): void;
+	}
+}
