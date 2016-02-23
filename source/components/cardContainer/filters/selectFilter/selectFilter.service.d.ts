a7de7214d9065adf2ad1d94cd95d2502f06e6b13
SamGraber <samgraber@sundialproperties.com>
2016-02-23 12:18:38 -0500

    Build

--- source/components/cardContainer/filters/selectFilter/selectFilter.service.d.ts
+++ source/components/cardContainer/filters/selectFilter/selectFilter.service.d.ts
@@ -3,9 +3,12 @@ export declare let factoryName: string;
 export interface ISelectFilter<T> extends filters.IFilter {
     selectedValue: any;
 }
+export interface IEqualityFunction<TFilterType> {
+    (item1: TFilterType, item2: TFilterType): boolean;
+}
 export interface ISelectFilterFactory {
     getInstance<T>(valueSelector: string | {
         (item: T): any;
-    }): ISelectFilter<T>;
+    }, comparer?: IEqualityFunction<T>): ISelectFilter<T>;
 }
 export declare function selectFilterFactory(): ISelectFilterFactory;
--- source/components/cardContainer/filters/selectFilter/selectFilter.service.js
+++ source/components/cardContainer/filters/selectFilter/selectFilter.service.js
@@ -1,16 +1,22 @@
 'use strict';
 var _ = require('lodash');
+var typescript_angular_utilities_1 = require('typescript-angular-utilities');
+var __object = typescript_angular_utilities_1.services.object;
 exports.factoryName = 'rlSelectFilterFactory';
 var SelectFilter = (function () {
-    function SelectFilter(valueSelector) {
+    function SelectFilter(valueSelector, comparer) {
         this.valueSelector = valueSelector;
+        this.comparer = comparer;
         this.type = 'selectFilter';
     }
     SelectFilter.prototype.filter = function (item) {
         if (this.selectedValue == null) {
             return true;
         }
-        return this.getValue(item) === this.selectedValue;
+        if (this.comparer != null) {
+            return this.comparer(this.getValue(item), this.selectedValue);
+        }
+        return __object.objectUtility.areEqual(this.getValue(item), this.selectedValue);
     };
     SelectFilter.prototype.getValue = function (item) {
         if (_.isFunction(this.valueSelector)) {
@@ -26,8 +32,8 @@ var SelectFilter = (function () {
 })();
 function selectFilterFactory() {
     return {
-        getInstance: function (valueSelector) {
-            return new SelectFilter(valueSelector);
+        getInstance: function (valueSelector, comparer) {
+            return new SelectFilter(valueSelector, comparer);
         },
     };
 }
