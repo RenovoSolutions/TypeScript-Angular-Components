6796a66a34b060efa581898d9fbbf300a3742f87
SamGraber <samgraber@sundialproperties.com>
2016-03-01 12:11:15 -0500

    Added support for specifying a template for the popover. By default, the popover expects a template property. (A property that binds to an html template) The consumer can set text-only="true" to bind in text content directly. (Also allows bindings)

--- source/behaviors/popover/popover.ts
+++ source/behaviors/popover/popover.ts
@@ -3,6 +3,7 @@
 import * as angular from 'angular';
 
 import { services } from 'typescript-angular-utilities';
+import __guid = services.guid;
 
 export var moduleName: string = 'rl.ui.behaviors.popover';
 export var directiveName: string = 'rlPopover';
@@ -10,17 +11,36 @@ export var controllerName: string = 'PopoverController';
 
 export interface IPopoverAttributes extends angular.IAttributes {
 	rlPopover: string;
+	textOnly: string;
 }
 
 export class PopoverController {
-	static $inject: string[] = ['$attrs', '$element', '$compile', '$scope'];
+	static $inject: string[] = ['$attrs'
+							, '$element'
+							, '$compile'
+							, '$scope'
+							, '$parse'
+							, '$templateCache'
+							, __guid.serviceName];
 	constructor(private $attrs: IPopoverAttributes
 			, private $element: angular.IAugmentedJQuery
 			, private $compile: angular.ICompileService
-			, private $scope: angular.IScope) { }
+			, private $scope: angular.IScope
+			, private $parse: angular.IParseService
+			, private $templateCache: angular.ITemplateCacheService
+			, private guid: __guid.IGuidService) { }
 
 	$onInit(): void {
-		this.$element.attr('uib-popover', this.$attrs.rlPopover);
+		if (this.$parse(this.$attrs.textOnly)(this.$scope)) {
+			this.$element.attr('uib-popover', this.$attrs.rlPopover);
+		}
+		else {
+			let templatePath: string = this.guid.random() + '.html';
+			let templateContent: string = this.$parse(this.$attrs.rlPopover)(this.$scope);
+			this.$templateCache.put(templatePath, templateContent);
+			this.$element.attr('uib-popover-template', '\'' + templatePath + '\'');
+		}
+
 		this.$element.removeAttr('rl-popover');
 		this.$compile(this.$element)(this.$scope);
 	}
