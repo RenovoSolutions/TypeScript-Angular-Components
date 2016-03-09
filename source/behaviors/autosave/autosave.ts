067c95bdb8b558a6cc17c6878429ab8108278f57
SamGraber <samgraber@sundialproperties.com>
2016-03-09 10:09:18 -0500

    Correct controller name is 'form'

--- source/behaviors/autosave/autosave.ts
+++ source/behaviors/autosave/autosave.ts
@@ -105,7 +105,7 @@ export function autosave(): angular.IDirective {
 		restrict: 'A',
 		require: {
 			autosaveController: 'rlAutosave',
-			form: '?ngForm',
+			form: '?form',
 		},
 		controller: controllerName,
 		bindToController: true,
