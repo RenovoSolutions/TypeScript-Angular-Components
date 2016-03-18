bc7bb7ba9292f5d4f2daa4f6c419a3e8558ae567
SamGraber <samgraber@sundialproperties.com>
2016-03-18 16:44:50 -0400

    Implemented onSubmitTrigger

--- source/services/autosave/triggers/onSubmitTrigger.ts
+++ source/services/autosave/triggers/onSubmitTrigger.ts
@@ -11,10 +11,7 @@ export interface OnSubmitSettings {
 }
 
 export class OnSubmitTrigger extends Trigger<OnSubmitSettings> implements ITrigger<OnSubmitSettings> {
-	private debounceDuration: number = 1000;
-	private timer: ng.IPromise<void>;
 	setListener: { (callback: IListener): IClearListener };
-	clearListener: IClearListener;
 
 	constructor() {
 		super('onSubmit');
@@ -27,51 +24,17 @@ export class OnSubmitTrigger extends Trigger<OnSubmitSettings> implements ITrigg
 
 		this.initListeners();
 
-		this.$rootScope.$watch((): boolean => {
-			return this.settings.form != null
-				? this.settings.form.$dirty
-				: false;
-		}, () => { this.triggerSaveAction(autosave); });
-
-		this.$rootScope.$watch((): boolean => {
-			return this.settings.form != null
-				? this.settings.form.$valid
-				: false;
-		}, () => { this.triggerSaveAction(autosave); });
-	}
-
-	private triggerSaveAction(autosave: {(): void}): void {
-		if (this.settings.form.$dirty && (this.settings.form.$valid || this.settings.saveWhenInvalid)) {
-			this.setTimer(autosave);
-
-			this.clearListener = this.setListener((): void => {
-				this.setTimer(autosave);
-			});
-		}
-	}
-
-	private setTimer(autosave: { (): void }): void {
-		if (this.timer != null) {
-			this.$timeout.cancel(this.timer);
-		}
-
-		this.timer = this.$timeout((): void => {
-			this.clearListener();
+		this.setListener((): void => {
 			autosave();
-		}, this.debounceDuration);
+		});
 	}
 
 	private initListeners(): void {
 		this.setListener = this.settings.setSubmitListener || this.nullSetListener;
-		this.clearListener = this.nullClearListener;
 	}
 
 	private nullSetListener(): IClearListener {
 		console.log('No submit listener available');
-		return this.nullClearListener;
-	}
-
-	private nullClearListener(): void {
-		console.log('No submit listener registered');
+		return null;
 	}
 }
