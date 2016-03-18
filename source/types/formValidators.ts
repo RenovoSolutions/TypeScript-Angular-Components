dd4e10af517ebd0c87140a2c3233d7404600ec89
SamGraber <samgraber@sundialproperties.com>
2016-03-18 17:02:09 -0400

    rlErrorMessage should be optional.

--- source/types/formValidators.ts
+++ source/types/formValidators.ts
@@ -3,9 +3,9 @@
 import * as angular from 'angular';
 
 export interface IFormValidator extends angular.IFormController {
-	rlErrorMessage: string;
+	rlErrorMessage?: string;
 }
 
 export interface INgModelValidator extends angular.INgModelController {
-	rlErrorMessage: string;
+	rlErrorMessage?: string;
 }
