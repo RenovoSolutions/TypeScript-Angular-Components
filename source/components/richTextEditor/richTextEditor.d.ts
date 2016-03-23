13ec22dee1fe37cb070c66bd3f1225286472e569
SamGraber <samgraber@sundialproperties.com>
2016-03-23 09:56:21 -0400

    Build

--- source/components/richTextEditor/richTextEditor.d.ts
+++ source/components/richTextEditor/richTextEditor.d.ts
@@ -4,22 +4,23 @@ import './editorButtons.css';
 import * as angular from 'angular';
 import { services } from 'typescript-angular-utilities';
 import __object = services.object;
-import { InputController } from '../input/input';
-import { IComponentValidatorFactory } from '../../services/componentValidator/componentValidator.service';
 import { IRichTextEditorProvider } from './richTextEditor.config';
 declare let externalProviderName: string;
 export { externalProviderName as providerName, IRichTextEditorProvider };
 export declare var moduleName: string;
-export declare var componentName: string;
+export declare var directiveName: string;
 export declare var controllerName: string;
 export interface IRichTextEditorBindings {
+    ngModel: string;
     customButtons: string[];
     ngDisabled: boolean;
 }
-export declare class RichTextEditorController extends InputController {
+export declare class RichTextEditorController {
+    ngModel: string;
     customButtons: string;
     ngDisabled: boolean;
     toolbar: string;
     static $inject: string[];
-    constructor($scope: angular.IScope, $attrs: angular.IAttributes, componentValidatorFactory: IComponentValidatorFactory, object: __object.IObjectUtility, provider: void);
+    constructor(object: __object.IObjectUtility, provider: void);
 }
+export declare function richTextEditor(): angular.IDirective;
