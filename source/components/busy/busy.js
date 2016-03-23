cdcddfdc3c93c2f336a5efc58b13b5f04214760f
SamGraber <samgraber@sundialproperties.com>
2016-03-23 13:21:03 -0400

    Build

--- source/components/busy/busy.js
+++ source/components/busy/busy.js
@@ -9,9 +9,9 @@ function busy(useDefaultTheme) {
     'use strict';
     return {
         restrict: 'E',
-        template: "<i class=\"busy rl-{{size}}\" ng-class=\"{ 'default-theme': useDefaultTheme }\" ng-show=\"loading\"></i>",
+        template: "<i class=\"busy rl-{{::size}}\" ng-class=\"{ 'default-theme': useDefaultTheme }\" ng-show=\"loading\"></i>",
         scope: {
-            loading: '=',
+            loading: '<',
             // Valid values are:
             // `lg`, `2x`, `3x`, `4x`, and `5x`
             size: '@',
