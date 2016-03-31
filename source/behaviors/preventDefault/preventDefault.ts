5035c288e3bd3c06ec44f1a958965e275cd1583b
theoriginaljosh <theoriginaljosh@gmail.com>
2016-03-31 13:21:05 -0400

    new prevent-default attribute directive that will prevent default browsers behaviour

---
+++ source/behaviors/preventDefault/preventDefault.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+import * as angular from 'angular';
+
+export let moduleName: string = 'rl.ui.behaviors.preventDefault';
+export let directiveName: string = 'rlPreventDefault';
+export let controllerName: string = 'PreventDefaultController';
+
+
+function required(): angular.IDirective {
+	return {
+		restrict: 'A',
+		priority: 200,
+		link: (scope: angular.IScope, element: angular.IAugmentedJQuery): void => {
+			angular.element(element).on('click', (event: any){
+				event.preventDefault();
+			});
+		}
+	};
+}
+
+angular.module(moduleName, [])
+	.directive(directiveName, required)
