6a543b6062ef6a0ca63740bdbba8dc15af6185e2
SamGraber <samgraber@sundialproperties.com>
2016-04-05 14:55:01 -0400

    Configure a json loader. This is needed for moment-timezone

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ var library = require('./webpack.library.json');
 
 var webpackRawLoader = require('./webpack.raw-loader');
 var webpackCssLoader = require('./webpack.css-loader');
+var webpackJsonLoader = require('./webpack.json-loader');
 
 var webpackConfig = webpack.library(library);
 webpackRawLoader(webpackConfig);
 webpackCssLoader(webpackConfig);
+webpackJsonLoader(webpackConfig);
 
 webpackConfig.devtool = "inline-source-map";
 
--- webpack.min.config.js
+++ webpack.min.config.js
@@ -3,9 +3,11 @@ var library = require('./webpack.library.json');
 
 var webpackRawLoader = require('./webpack.raw-loader');
 var webpackCssLoader = require('./webpack.css-loader');
+var webpackJsonLoader = require('./webpack.json-loader');
 
 var webpackConfig = webpack.libraryMin(library);
 webpackRawLoader(webpackConfig);
 webpackCssLoader(webpackConfig);
+webpackJsonLoader(webpackConfig);
 
 module.exports = webpackConfig;
