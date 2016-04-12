134ad92aacfdfcb7d6dda259095fb5788a46edf8
SamGraber <samgraber@sundialproperties.com>
2016-04-12 09:41:27 -0400

    Updated buttonSubmit and buttonToggle to inherit from the base button.

--- source/components/buttonSubmit/buttonSubmit.ts
+++ source/components/buttonSubmit/buttonSubmit.ts
@@ -4,43 +4,19 @@
 
 import * as angular from 'angular';
 
-import { componentName as formComponentName, FormController } from '../form/form';
+import { buildButton } from '../button/button';
 
 export let moduleName: string = 'rl.ui.components.buttonSubmit';
 export let componentName: string = 'rlButtonSubmit';
-export let controllerName: string = 'ButtonSubmitController';
 
-export class ButtonSubmitController {
-	// bindings
-	type: string;
-	ngDisabled: boolean;
-	size: string;
-	rightAligned: boolean;
-	saving: boolean;
-
-	configuredSize: string;
-
-	static $inject: string[] = ['$element'];
-	constructor(private $element: angular.IAugmentedJQuery) {
-		this.type = this.type != null ? this.type : 'default';
-		this.configuredSize = this.size != null ? 'btn-' + this.size : null;
-	}
-}
-
-let buttonSubmit: angular.IComponentOptions = {
-		transclude: true,
+let buttonSubmit: angular.IComponentOptions = buildButton({
 		template: require('./buttonSubmit.html'),
 		bindings: {
-			type: '@',
-			ngDisabled: '<?',
-			size: '@',
 			rightAligned: '<?',
 			saving: '<?',
+			action: null,
 		},
-		controller: controllerName,
-		controllerAs: 'button',
-	};
+	});
 
 angular.module(moduleName, [])
-	.component(componentName, buttonSubmit)
-	.controller(controllerName, ButtonSubmitController);
+	.component(componentName, buttonSubmit);
