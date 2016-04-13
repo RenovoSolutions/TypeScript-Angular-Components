5810890a390ccbecb5015eac3dea6f166c32b19a
Josh Graber <jgraber@renovo1.com>
2016-04-13 14:27:42 -0400

    Update to the latest version of gulp utilities and remove loaders as they are now provided as defaults.

--- karma.shared.conf.js
+++ karma.shared.conf.js
@@ -1,9 +1,5 @@
 // Karma default configuration
 
-var webpackRawLoader = require('./webpack.raw-loader');
-var webpackCssLoader = require('./webpack.css-loader');
-var webpackJsonLoader = require('./webpack.json-loader');
-
 module.exports = function (karma, karmaSettings) {
 	var karmaConfig = karmaSettings(karma, 'test-bootstrapper.js', [
 		'./node_modules/jquery/dist/jquery.js',
@@ -16,10 +12,6 @@ module.exports = function (karma, karmaSettings) {
 		'moment': 'moment',
 		'moment-timezone': 'moment',
 	});
-
-	webpackRawLoader(karmaConfig.webpack);
-	webpackCssLoader(karmaConfig.webpack);
-	webpackJsonLoader(karmaConfig.webpack);
 
 	karma.set(karmaConfig);
 	return karmaConfig;
--- webpack.css-loader.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (webpackConfig) {
-	webpackConfig.module = webpackConfig.module || {};
-	webpackConfig.module.loaders = webpackConfig.module.loaders || [];
-
-	webpackConfig.module.loaders.push({
-		test: /\.css$/,
-		loader: 'style-loader!css-loader',
-	});
-};
