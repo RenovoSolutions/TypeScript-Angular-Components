153a25903ef34bc7b4804e4edf0f1e5cb4b407bc
SamGraber <samgraber@sundialproperties.com>
2016-04-15 14:18:08 -0400

    Test paging with 100 items

--- bootstrapper/cards/cardContainerBootstrapper.ts
+++ bootstrapper/cards/cardContainerBootstrapper.ts
@@ -1,4 +1,5 @@
 import * as angular from 'angular';
+import * as _ from 'lodash';
 
 import { factoryName as builderService, ICardContainerBuilderFactory, ICardContainerBuilder } from '../../source/components/cardContainer/cardContainerBuilder.service';
 
@@ -12,17 +13,13 @@ class CardTestController {
 
 	static $inject: string[] = [builderService];
 	constructor(cardContainerBuilderFactory: ICardContainerBuilderFactory) {
-		const items: ICardItem[] = [
-			{ name: 'Item 1', value: 1 },
-			{ name: 'Item 2', value: 2 },
-			{ name: 'Item 3', value: 1 },
-			{ name: 'Item 4', value: 1 },
-			{ name: 'Item 5', value: 2 },
-			{ name: 'Item 6', value: 2 },
-		];
+		const items: ICardItem[] = _.map(_.range(1, 100), (num: number): ICardItem => {
+			return { name: 'Item' + num, value: Math.floor(Math.random() * 2) + 1 };
+		});
 
 		this.builder = cardContainerBuilderFactory.getInstance();
 		this.builder.dataSource.buildSimpleDataSource(items);
+		this.builder.usePaging();
 		this.builder.addColumn({
 			label: 'Name',
 			size: 6,
