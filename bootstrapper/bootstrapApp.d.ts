70f564321405d11dc9b9c59b941e47c9d9d764d9
SamGraber <samgraber@sundialproperties.com>
2016-04-18 13:26:11 -0400

    Build-bundle

--- bootstrapper/bootstrapApp.d.ts
+++
@@ -1,9 +0,0 @@
-declare const inputsTemplate: any;
-declare const buttonsTemplate: any;
-declare const popupTemplate: any;
-declare const cardsTemplate: any;
-declare const tabsTemplate: any;
-declare const formsTemplate: any;
-declare const messageLogTemplate: any;
-declare const miscTemplate: any;
-declare function RouteConfig($urlRouterProvider: any, $stateProvider: any): void;
