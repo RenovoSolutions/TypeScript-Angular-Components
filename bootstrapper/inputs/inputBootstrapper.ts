0d6c1c3ddc309c73491faad762d1fb97b33c334b
SamGraber <samgraber@sundialproperties.com>
2016-04-19 11:25:44 -0400

    Added validation samples to the input bootstrapper.

--- bootstrapper/inputs/inputBootstrapper.ts
+++ bootstrapper/inputs/inputBootstrapper.ts
@@ -4,6 +4,7 @@ import 'moment-timezone';
 
 import { services } from 'typescript-angular-utilities';
 import __timezone = services.timezone;
+import __validation = services.validation;
 
 interface ITestItem {
 	name: string;
@@ -15,6 +16,11 @@ class InputTestController {
 	typeaheadList: ITestItem[];
 	date: moment.Moment;
 	date2: moment.Moment;
+	validate1: number;
+	validate2: number;
+	greaterThanFive1: __validation.IValidationHandler;
+	greaterThanFive2: __validation.IValidationHandler;
+	lessThanTen: __validation.IValidationHandler;
 
 	static $inject: string[] = ['$q'];
 	constructor(private $q: angular.IQService) {}
@@ -32,6 +38,19 @@ class InputTestController {
 
 		this.date = moment('2016-04-01T12:00:00.000-08:00').tz('US/Pacific');
 		__timezone.timezoneService.setCurrentTimezone('-08:00');
+
+		this.greaterThanFive1 = {
+			validate: (): boolean => this.validate1 > 5,
+			errorMessage: 'Must be greater than 5',
+		};
+		this.greaterThanFive2 = {
+			validate: (): boolean => this.validate2 > 5,
+			errorMessage: 'Must be greater than 5',
+		};
+		this.lessThanTen = {
+			validate: (): boolean => this.validate2 < 10,
+			errorMessage: 'Must be less than 10',
+		};
 	}
 
 	select(value: ITestItem): void {
