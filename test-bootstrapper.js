178ef34d5fa2355262b1fdef31170691c3a666a1
Josh Graber <jgraber@renovo1.com>
2016-04-28 16:09:01 -0400

    Don't need to call downgrade function in config block.

--- test-bootstrapper.js
+++ test-bootstrapper.js
@@ -1,17 +1,18 @@
+require('core-js/es6');
+require('core-js/es7/reflect');
+
 var angular = require('angular');
-var UpgradeAdapter = require('angular2/upgrade');
+var UpgradeAdapter = require('angular2/upgrade').UpgradeAdapter;
 var adapter = new UpgradeAdapter();
 
-var downgrade = require('typescript-angular-utilities').downgrade;
+var utilities = require('typescript-angular-utilities');
+var downgrade = utilities.downgrade;
 
 var testModuleName = 'test-bootstrapper';
 
-angular.module(testModuleName, [])
-	.config(function () {
-		downgrade.downgradeUtilitiesToAngular1(adapter);
-	});
+downgrade.downgradeUtilitiesToAngular1(adapter);
 
-angular.mock.module(testModuleName);
+angular.mock.module(downgrade.moduleName);
 
 // require all modules ending in ".tests.ts" from source and all subdirectories
 var testsContext = require.context('./source', true, /\.tests\.js$/);
