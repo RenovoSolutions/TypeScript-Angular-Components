94ad1cb5aa07cb3937e921496a3a8d3da6995c7a
Josh Graber <jgraber@renovo1.com>
2016-04-28 15:17:15 -0400

    Updated the test bootstrapper to use the upgrade adapter.

--- test-bootstrapper.js
+++ test-bootstrapper.js
@@ -1,4 +1,18 @@
-// require all modules ending in ".tests.ts" from source and all subdirectories
+var angular = require('angular');
+var UpgradeAdapter = require('angular2/upgrade');
+var adapter = new UpgradeAdapter();
 
+var downgrade = require('typescript-angular-utilities').downgrade;
+
+var testModuleName = 'test-bootstrapper';
+
+angular.module(testModuleName, [])
+	.config(function () {
+		downgrade.downgradeUtilitiesToAngular1(adapter);
+	});
+
+angular.mock.module(testModuleName);
+
+// require all modules ending in ".tests.ts" from source and all subdirectories
 var testsContext = require.context('./source', true, /\.tests\.js$/);
 testsContext.keys().forEach(testsContext);
