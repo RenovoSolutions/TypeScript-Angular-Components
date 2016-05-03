0de1428dd1b2e13667810dbe85c02049553b5bfb
SamGraber <samgraber@sundialproperties.com>
2016-05-03 15:55:56 -0400

    Added a helper method for getting the html value of a jquery element. Wraps the element in a div and calls .html(). Normally, calling .html() causes the outer tag to get lost.

--- source/services/jquery/jquery.service.ts
+++ source/services/jquery/jquery.service.ts
@@ -8,10 +8,15 @@ export var moduleName: string = 'rl.utilities.services.jquery';
 export var serviceName: string = 'jqueryUtility';
 
 export interface IJQueryUtility {
+	getHtml(jquery: JQuery): string;
 	replaceContent(contentArea: JQuery, newContents: JQuery): JQuery;
 }
 
 class JQueryUtility implements IJQueryUtility {
+	getHtml(jquery: JQuery): string {
+		return angular.element('<div>').append(jquery).html()
+	}
+
 	replaceContent(contentArea: JQuery, newContent: JQuery): JQuery {
 		contentArea.empty();
 		return contentArea.append(newContent);
