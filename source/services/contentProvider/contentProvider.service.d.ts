bba92cd0725b652fbc40db532a74b4183132588d
SamGraber <samgraber@sundialproperties.com>
2016-05-09 11:14:20 -0400

    Build-bundle

--- source/services/contentProvider/contentProvider.service.d.ts
+++ source/services/contentProvider/contentProvider.service.d.ts
@@ -1,5 +1,5 @@
 import * as ng from 'angular';
-import { Subject, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 export declare var moduleName: string;
 export declare var serviceName: string;
 export interface IContentChanges {
@@ -7,13 +7,10 @@ export interface IContentChanges {
     scope?: ng.IScope;
 }
 export interface IContentProviderService {
-    contentChanges: Subject<IContentChanges>;
+    contentChanges: Observable<IContentChanges>;
     setContent(content: JQuery): void;
     setTranscludeContent(transcludeFunction: ng.ITranscludeFunction): void;
     getContent(selector?: string): JQuery;
-    subscribe(action: {
-        (newText: JQuery): void;
-    }, selector?: string): Subscription;
 }
 export interface IContentProviderServiceFactory {
     getInstance(): IContentProviderService;
