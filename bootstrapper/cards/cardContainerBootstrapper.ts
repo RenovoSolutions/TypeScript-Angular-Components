0452faab2ab57bd31c3ead9be4b798b7402449b1
SamGraber <samgraber@sundialproperties.com>
2016-05-09 09:47:51 -0400

    Add an example of a select filter with a dynamic template.

--- bootstrapper/cards/cardContainerBootstrapper.ts
+++ bootstrapper/cards/cardContainerBootstrapper.ts
@@ -1,7 +1,13 @@
 import * as angular from 'angular';
 import * as _ from 'lodash';
 
-import { factoryName as builderService, ICardContainerBuilderFactory, ICardContainerBuilder } from '../../source/components/cardContainer/cardContainerBuilder.service';
+import {
+	factoryName as builderService,
+	ICardContainerBuilderFactory,
+	ICardContainerBuilder,
+	ISelectFilter,
+	IDataSource,
+} from '../../source/components/cardContainer/cardContainerBuilder.service';
 
 export const moduleName: string = 'CardTestModule';
 
@@ -12,12 +18,18 @@ interface ICardItem {
 
 class CardTestController {
 	builder: ICardContainerBuilder;
+	builderWithSelectFilter: ICardContainerBuilder;
+	options: number[];
+	selectFilter: ISelectFilter<number>;
+	dataSource: IDataSource<ICardItem>;
 
 	static $inject: string[] = [builderService];
 	constructor(cardContainerBuilderFactory: ICardContainerBuilderFactory) {
 		const items: ICardItem[] = _.map(_.range(1, 101), (num: number): ICardItem => {
 			return { name: 'Item' + num, value: Math.floor(Math.random() * 2) + 1 };
 		});
+
+		this.options = [1, 2];
 
 		this.builder = cardContainerBuilderFactory.getInstance();
 		this.builder.dataSource.buildSimpleDataSource(items);
@@ -52,6 +64,24 @@ class CardTestController {
 					value: 2,
 				},
 			],
+		});
+
+		this.builderWithSelectFilter = cardContainerBuilderFactory.getInstance();
+		this.dataSource = this.builderWithSelectFilter.dataSource.buildSimpleDataSource(items);
+		this.builderWithSelectFilter.usePaging();
+		this.builderWithSelectFilter.addColumn({
+			label: 'Name',
+			size: 6,
+			getValue: 'name',
+		});
+		this.builderWithSelectFilter.addColumn({
+			label: 'Value',
+			size: 6,
+			getValue: 'value',
+			template: '<b>{{myItem.value}}</b>',
+		});
+		this.selectFilter = this.builderWithSelectFilter.filters.buildSelectFilter({
+			valueSelector: 'value',
 		});
 	}
 }
