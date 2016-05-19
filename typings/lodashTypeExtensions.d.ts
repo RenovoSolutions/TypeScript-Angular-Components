948b8e98d4b76deb535cd66c97d90cbfc310b151
Josh Graber <jgraber@renovo1.com>
2016-05-19 11:07:51 -0400

    Switch from tsd to typings.

--- typings/lodashTypeExtensions.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path='lodash/lodash.d.ts' />
-
-declare module _ {
-	interface LoDashArrayWrapper<T> extends LoDashWrapperBase<T[], LoDashArrayWrapper<T>> {
-		drop<T>(num: number): LoDashArrayWrapper<T>;
-	}
-}
