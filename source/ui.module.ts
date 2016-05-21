7da664e514cd6f797c9c973b813377a86aae29b8
SamGraber <samgraber@sundialproperties.com>
2016-05-21 15:07:02 -0400

    Created a downgrader for components with a placeholder angular 2 textbox.

--- source/ui.module.ts
+++ source/ui.module.ts
@@ -4,7 +4,7 @@ import 'angular-ui-bootstrap';
 import 'angular-sanitize';
 import 'angular-animate';
 
-import { downgrade } from 'typescript-angular-utilities';
+import { downgrade as utilitiesDowngrade } from 'typescript-angular-utilities';
 
 import '../libraries/angular-bootstrap-slider/slider';
 
@@ -12,11 +12,12 @@ import 'signature_pad';
 
 import * as behaviors from './behaviors/behaviors.module';
 import * as components from './components/components.module';
+import * as downgrade from './componentsDowngrade';
 import * as filters from './filters/filters.module';
 import * as services from './services/services.module';
 import * as types from './types/types.module';
 
-export { behaviors, components, filters, services, types };
+export { behaviors, components, downgrade, filters, services, types };
 
 export var moduleName: string = 'rl.ui';
 
@@ -24,10 +25,11 @@ angular.module(moduleName, [
 	'ui.bootstrap',
 	'ui.bootstrap-slider',
 	'ngSanitize',
-	downgrade.moduleName,
+	utilitiesDowngrade.moduleName,
 
 	behaviors.moduleName,
 	components.moduleName,
+	downgrade.moduleName,
 	filters.moduleName,
 	services.moduleName,
 ]);
