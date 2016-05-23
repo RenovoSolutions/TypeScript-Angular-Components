146b193eb2c4aae6b0200c3c4972877d4c1d4d33
SamGraber <samgraber@sundialproperties.com>
2016-05-23 18:14:05 -0400

    Created a toggle button angular 2 component.

--- source/componentsDowngrade.ts
+++ source/componentsDowngrade.ts
@@ -6,6 +6,7 @@ import * as angular from 'angular';
 import { ButtonComponent } from './components/button/button.ng2';
 import { ButtonLinkComponent } from './components/buttonLink/buttonLink.ng2';
 import { ButtonSubmitComponent } from './components/buttonSubmit/buttonSubmit.ng2';
+import { ButtonToggleComponent } from './components/buttonToggle/buttonToggle.ng2';
 import { CheckboxComponent } from './components/checkbox/checkbox.ng2';
 import { FormComponent } from './components/form/form.ng2';
 import { TextboxComponent } from './components/textbox/textbox.ng2';
@@ -15,6 +16,7 @@ import { defaultThemeToken, defaultThemeValueName, DEFAULT_THEME_PROVIDER } from
 export const buttonComponentName: string = 'rlButtonNg';
 export const buttonLinkComponentName: string = 'rlButtonLinkNg';
 export const buttonSubmitComponentName: string = 'rlButtonSubmitNg';
+export const buttonToggleComponentName: string = 'rlButtonToggleNg';
 export const checkboxComponentName: string = 'rlCheckboxNg';
 export const formComponentName: string = 'rlFormNg';
 export const textboxComponentName: string = 'rlTextboxNg';
@@ -31,6 +33,7 @@ export function downgradeComponentsToAngular1(upgradeAdapter: UpgradeAdapter) {
 	componentsDowngradeModule.directive(buttonComponentName, <any>upgradeAdapter.downgradeNg2Component(ButtonComponent));
 	componentsDowngradeModule.directive(buttonLinkComponentName, <any>upgradeAdapter.downgradeNg2Component(ButtonLinkComponent));
 	componentsDowngradeModule.directive(buttonSubmitComponentName, <any>upgradeAdapter.downgradeNg2Component(ButtonSubmitComponent));
+	componentsDowngradeModule.directive(buttonToggleComponentName, <any>upgradeAdapter.downgradeNg2Component(ButtonToggleComponent));
 	componentsDowngradeModule.directive(checkboxComponentName, <any>upgradeAdapter.downgradeNg2Component(CheckboxComponent));
 	componentsDowngradeModule.directive(formComponentName, <any>upgradeAdapter.downgradeNg2Component(FormComponent));
 	componentsDowngradeModule.directive(textboxComponentName, <any>upgradeAdapter.downgradeNg2Component(TextboxComponent));
