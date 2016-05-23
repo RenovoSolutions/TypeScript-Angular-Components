cf0ae14833b94b781afb474891c371cb1461e7a7
SamGraber <samgraber@sundialproperties.com>
2016-05-23 10:11:01 -0400

    Added placeholder submit button and form components. I'll use these to begin testing how forms can work in angular 2.

--- source/componentsDowngrade.ts
+++ source/componentsDowngrade.ts
@@ -3,8 +3,12 @@ import { UpgradeAdapter } from '@angular/upgrade';
 
 import * as angular from 'angular';
 
+import { ButtonSubmitComponent } from './components/buttonSubmit/buttonSubmit.ng2';
+import { FormComponent } from './components/form/form.ng2';
 import { TextboxComponent } from './components/textbox/textbox.ng2';
 
+export const buttonSubmitComponentName: string = 'rlButtonSubmitNg';
+export const formComponentName: string = 'rlFormNg';
 export const textboxComponentName: string = 'rlTextboxNg';
 
 export const moduleName: string = 'rl.components.downgrade';
@@ -12,5 +16,7 @@ export const moduleName: string = 'rl.components.downgrade';
 const componentsDowngradeModule = angular.module(moduleName, []);
 
 export function downgradeComponentsToAngular1(upgradeAdapter: UpgradeAdapter) {
+	componentsDowngradeModule.directive(buttonSubmitComponentName, <any>upgradeAdapter.downgradeNg2Component(ButtonSubmitComponent));
+	componentsDowngradeModule.directive(formComponentName, <any>upgradeAdapter.downgradeNg2Component(FormComponent));
 	componentsDowngradeModule.directive(textboxComponentName, <any>upgradeAdapter.downgradeNg2Component(TextboxComponent));
 }
