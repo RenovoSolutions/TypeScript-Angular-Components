717c4427b03b10762d18427ceddcc6218acc003d
SamGraber <samgraber@sundialproperties.com>
2016-05-24 13:51:06 -0400

    Implemented a spinner for submit buttons.

--- source/components/form/form.ng2.ts
+++ source/components/form/form.ng2.ts
@@ -1,16 +1,29 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, Input } from '@angular/core';
 import { NgForm } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import { IWaitValue } from '../busy/busy.ng2';
+
+export interface ISaveAction<T> {
+	(data: T): IWaitValue<T>;
+}
 
 @Component({
 	selector: 'rlForm',
 	template: require('./form.ng2.html'),
 })
 export class FormComponent {
+	@Input() save: ISaveAction<any>;
+
 	@ViewChild('form') form: NgForm;
 
-	submit(): Promise<void> {
-		console.log('Submit');
-		console.log(this.form);
-		return Promise.resolve();
+	constructor() {
+		if (!this.save) {
+			this.save = <ISaveAction>() => Promise.resolve();
+		}
+	}
+
+	submit(): IWaitValue<any> {
+		return this.save(this.form.value);
 	}
 }
