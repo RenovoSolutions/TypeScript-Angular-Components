dbaefb8977de820d66a7cdfceee231d38c52fd1a
SamGraber <samgraber@sundialproperties.com>
2016-05-24 08:28:46 -0400

    Created an example angular 2 form and established a relationship between the form and the submit button.

--- bootstrapper/forms/formsNg2Bootstrapper.ts
+++ bootstrapper/forms/formsNg2Bootstrapper.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
+import { FormComponent } from '../../source/components/form/form.ng2';
+import { ButtonSubmitComponent } from '../../source/components/buttonSubmit/buttonSubmit.ng2';
+import { TextboxComponent } from '../../source/components/textbox/textbox.ng2';
+
 @Component({
 	selector: 'tsFormsBootstrapper',
 	template: require('./formsNg2.html'),
+	directives: [FormComponent, ButtonSubmitComponent, TextboxComponent],
 })
 export class FormsBootstrapper {
 
