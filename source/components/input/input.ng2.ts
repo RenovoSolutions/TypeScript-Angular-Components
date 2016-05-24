3fbc57a1e6df4504aed0fd7f8aa1186abac9a98d
SamGraber <samgraber@sundialproperties.com>
2016-05-24 15:32:01 -0400

    Moved the heavy lifting of input behavior to a base class for inputs. We may need to tweak this as we see what behavior is shared by other inputs and which is specific to textboxes.

---
+++ source/components/input/input.ng2.ts
@@ -0,0 +1,61 @@
+import { Component, ViewChild, Optional, AfterViewInit } from '@angular/core';
+import { Control, NgControl, NgFormControl, FORM_DIRECTIVES } from '@angular/common';
+import { first, values } from 'lodash';
+
+import { services } from 'typescript-angular-utilities';
+import __validation = services.validation;
+import __object = services.object;
+
+import { FormComponent } from '../form/form.ng2';
+
+export const baseInputs: string[] = ['validator', 'validators', 'label', 'name'];
+
+export class InputComponent implements AfterViewInit {
+	validator: __validation.IValidationHandler;
+	validators: __validation.IValidationHandler[];
+	label: string;
+	name: string;
+
+	error: string;
+	control: Control;
+	rlForm: FormComponent;
+
+	constructor(@Optional() rlForm: FormComponent) {
+		this.rlForm = rlForm;
+		this.control = new Control('', buildValidator(<any>[{
+			name: 'rlRequired',
+			validate: (value: any): boolean => { return !__object.objectUtility.isNullOrEmpty(value); },
+			errorMessage: 'Required field',
+		}]));
+		this.control.statusChanges.subscribe((value: any): void => {
+			this.setError();
+		});
+		this.name = 'test123';
+	}
+
+	ngAfterViewInit(): void {
+		this.setError();
+		if (this.rlForm) {
+			this.rlForm.form.addControl(this.name, this.control);
+		}
+	}
+
+	setError(): string {
+		if (!this.control) {
+			return;
+		}
+
+		this.error = <any>first(values(this.control.errors));
+	}
+}
+
+function buildValidator(validators: __validation.IValidationHandler[]): any {
+	return (control: Control): any => {
+		if ((<any>validators[0]).validate(control.value)) {
+			return null;
+		}
+		let errors: any = {};
+		errors[validators[0].name] = validators[0].errorMessage;
+		return errors;
+	};
+}
