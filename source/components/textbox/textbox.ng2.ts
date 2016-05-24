432ea762d67c4b336f1534f6cba812fbd06d8264
SamGraber <samgraber@sundialproperties.com>
2016-05-24 15:23:56 -0400

    Working implementation of a textbox input. Right now it seems to work better to define the form as a control group and then use ngFormModel to apply it to the page. That way we can do the same for the inputs, and set validators programmatically. The alternative would be to create a custom directive for our validators, but that doesn't seem worthwhile in this case.

--- source/components/textbox/textbox.ng2.ts
+++ source/components/textbox/textbox.ng2.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Optional, AfterViewInit } from '@angular/core';
+import { Control, NgControl, NgFormControl, FORM_DIRECTIVES } from '@angular/common';
+import { first, values } from 'lodash';
+
+import { services } from 'typescript-angular-utilities';
+import __validation = services.validation;
+import __object = services.object;
+
+import { FormComponent } from '../form/form.ng2';
+
+export const baseInputs: string[] = ['validator', 'validators', 'label', 'name'];
 
 @Component({
 	selector: 'rlTextbox',
 	template: require('./textbox.ng2.html'),
+	inputs: baseInputs,
+	directives: [FORM_DIRECTIVES],
 })
-export class TextboxComponent {
-	constructor() {
-		console.log('Component');
+export class TextboxComponent implements AfterViewInit {
+	validator: __validation.IValidationHandler;
+	validators: __validation.IValidationHandler[];
+	label: string;
+	name: string;
+
+	error: string;
+	control: Control;
+	rlForm: FormComponent;
+
+	@ViewChild('input') input: NgFormControl;
+
+	constructor(@Optional() rlForm: FormComponent) {
+		this.rlForm = rlForm;
+		this.control = new Control('', buildValidator(<any>[{
+			name: 'rlRequired',
+			validate: (value: any): boolean => { return !__object.objectUtility.isNullOrEmpty(value); },
+			errorMessage: 'Required field',
+		}]));
+		this.control.statusChanges.subscribe((value: any): void => {
+			this.setError();
+		});
+		this.name = 'test123';
 	}
+
+	ngAfterViewInit(): void {
+		this.setError();
+		if (this.rlForm) {
+			this.rlForm.form.addControl(this.name, this.control);
+		}
+	}
+
+	setError(): string {
+		if (!this.control) {
+			return;
+		}
+
+		this.error = <any>first(values(this.control.errors));
+	}
+}
+
+function buildValidator(validators: __validation.IValidationHandler[]): any {
+	return (control: Control): any => {
+		if ((<any>validators[0]).validate(control.value)) {
+			return null;
+		}
+		let errors: any = {};
+		errors[validators[0].name] = validators[0].errorMessage;
+		return errors;
+	};
 }
