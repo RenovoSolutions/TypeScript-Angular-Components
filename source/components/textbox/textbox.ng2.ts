70d0215f22bdef55b857deb253a8e83d5b18d51e
SamGraber <samgraber@sundialproperties.com>
2016-05-24 15:33:33 -0400

    Confirmed that you don't have to explicitly specify the form directives as being used.

--- source/components/textbox/textbox.ng2.ts
+++ source/components/textbox/textbox.ng2.ts
@@ -1,5 +1,4 @@
 import { Component, Optional } from '@angular/core';
-import { FORM_DIRECTIVES } from '@angular/common';
 
 import { InputComponent, baseInputs } from '../input/input.ng2';
 import { FormComponent } from '../form/form.ng2';
@@ -8,7 +7,6 @@ import { FormComponent } from '../form/form.ng2';
 	selector: 'rlTextbox',
 	template: require('./textbox.ng2.html'),
 	inputs: baseInputs,
-	directives: [FORM_DIRECTIVES],
 })
 export class TextboxComponent extends InputComponent {
 	constructor(@Optional() rlForm: FormComponent) {
