16e479e2df01f652b761247ccf7ea9a2916dbb91
SamGraber <samgraber@sundialproperties.com>
2016-05-24 13:00:02 -0400

    Moved button display functionality to a BaseButtonComponent. In reality, most buttons don't use the trigger logic, particularly since async buttons now have to work differently. The only thing we want to share is the size, type, and disabled configuration.

--- source/components/button/button.ng2.ts
+++ source/components/button/button.ng2.ts
@@ -1,37 +1,12 @@
-import { Component, EventEmitter } from '@angular/core';
-import { each } from 'lodash';
+import { Component, EventEmitter, Output } from '@angular/core';
 
-export const baseInputs: string[] = ['type', 'disabled', 'size'];
-export const baseOutputs: string[] = ['trigger'];
+import { BaseButtonComponent, baseInputs } from './baseButton.ng2';
 
 @Component({
 	selector: 'rlButton',
 	template: require('./button.ng2.html'),
 	inputs: baseInputs,
-	outputs: baseOutputs,
 })
-export class ButtonComponent {
-	type: string;
-	disabled: boolean;
-	size: string;
-	trigger: EventEmitter<any> = new EventEmitter();
-
-	get configuredSize(): string {
-		return this.size != null && this.size !== ''
-			? 'btn-' + this.size
-			: null;
-	}
-
-	get configuredTypes(): string {
-		let type: string = this.type || 'default';
-		let typesList: string[] = type.split(' ');
-		each(typesList, (type: string, index: number): void => {
-			//the for each for places that used btn-block for example in the type attribute do not break
-			if (type.indexOf('btn-') === -1) {
-				type = 'btn-' + type;
-			}
-			typesList[index] = type;
-		});
-		return typesList.join(' ');
-	}
+export class ButtonComponent extends BaseButtonComponent {
+	@Output() trigger: EventEmitter<any> = new EventEmitter();
 }
--- source/components/buttonLink/buttonLink.ng2.ts
+++ source/components/buttonLink/buttonLink.ng2.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 
-import { ButtonComponent, baseInputs } from '../button/button.ng2';
+import { BaseButtonComponent, baseInputs } from '../button/baseButton.ng2';
 
 @Component({
 	selector: 'rlButtonLink',
 	template: require('./buttonLink.ng2.html'),
 	inputs: baseInputs,
 })
-export class ButtonLinkComponent extends ButtonComponent {
+export class ButtonLinkComponent extends BaseButtonComponent {
 	@Input() link: string;
 	@Input() newTab: boolean;
 
