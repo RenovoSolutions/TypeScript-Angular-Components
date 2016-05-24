c2fe73cfd3ac4d31bf43ae73c4ed16d83d6863ab
SamGraber <samgraber@sundialproperties.com>
2016-05-24 15:32:01 -0400

    Moved the heavy lifting of input behavior to a base class for inputs. We may need to tweak this as we see what behavior is shared by other inputs and which is specific to textboxes.

--- source/components/textbox/textbox.ng2.ts
+++ source/components/textbox/textbox.ng2.ts
@@ -1,14 +1,8 @@
-import { Component, ViewChild, Optional, AfterViewInit } from '@angular/core';
-import { Control, NgControl, NgFormControl, FORM_DIRECTIVES } from '@angular/common';
-import { first, values } from 'lodash';
+import { Component, Optional } from '@angular/core';
+import { FORM_DIRECTIVES } from '@angular/common';
 
-import { services } from 'typescript-angular-utilities';
-import __validation = services.validation;
-import __object = services.object;
-
+import { InputComponent, baseInputs } from '../input/input.ng2';
 import { FormComponent } from '../form/form.ng2';
-
-export const baseInputs: string[] = ['validator', 'validators', 'label', 'name'];
 
 @Component({
 	selector: 'rlTextbox',
@@ -16,54 +10,8 @@ export const baseInputs: string[] = ['validator', 'validators', 'label', 'name']
 	inputs: baseInputs,
 	directives: [FORM_DIRECTIVES],
 })
-export class TextboxComponent implements AfterViewInit {
-	validator: __validation.IValidationHandler;
-	validators: __validation.IValidationHandler[];
-	label: string;
-	name: string;
-
-	error: string;
-	control: Control;
-	rlForm: FormComponent;
-
-	@ViewChild('input') input: NgFormControl;
-
+export class TextboxComponent extends InputComponent {
 	constructor(@Optional() rlForm: FormComponent) {
-		this.rlForm = rlForm;
-		this.control = new Control('', buildValidator(<any>[{
-			name: 'rlRequired',
-			validate: (value: any): boolean => { return !__object.objectUtility.isNullOrEmpty(value); },
-			errorMessage: 'Required field',
-		}]));
-		this.control.statusChanges.subscribe((value: any): void => {
-			this.setError();
-		});
-		this.name = 'test123';
+		super(rlForm);
 	}
-
-	ngAfterViewInit(): void {
-		this.setError();
-		if (this.rlForm) {
-			this.rlForm.form.addControl(this.name, this.control);
-		}
-	}
-
-	setError(): string {
-		if (!this.control) {
-			return;
-		}
-
-		this.error = <any>first(values(this.control.errors));
-	}
-}
-
-function buildValidator(validators: __validation.IValidationHandler[]): any {
-	return (control: Control): any => {
-		if ((<any>validators[0]).validate(control.value)) {
-			return null;
-		}
-		let errors: any = {};
-		errors[validators[0].name] = validators[0].errorMessage;
-		return errors;
-	};
 }
