a37e0bfd27ce2af877bd9b391ba7e567d79e2a94
SamGraber <samgraber@sundialproperties.com>
2016-05-24 08:47:24 -0400

    Created a base valueAccessor class that we can use to implement ngModel behavior in components. Used it to implement ngModel for checkboxes. It looks like this can only be used in a form, because ngModel requires a parent form component.

--- bootstrapper/forms/formsNg2Bootstrapper.ts
+++ bootstrapper/forms/formsNg2Bootstrapper.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 
 import { FormComponent } from '../../source/components/form/form.ng2';
 import { ButtonSubmitComponent } from '../../source/components/buttonSubmit/buttonSubmit.ng2';
+import { CheckboxComponent } from '../../source/components/checkbox/checkbox.ng2';
 import { TextboxComponent } from '../../source/components/textbox/textbox.ng2';
 
 @Component({
 	selector: 'tsFormsBootstrapper',
 	template: require('./formsNg2.html'),
-	directives: [FormComponent, ButtonSubmitComponent, TextboxComponent],
+	directives: [
+		FormComponent,
+		ButtonSubmitComponent,
+		CheckboxComponent,
+		TextboxComponent,
+	],
 })
 export class FormsBootstrapper {
-
+	checked: boolean;
 }
--- source/components/checkbox/checkbox.ng2.ts
+++ source/components/checkbox/checkbox.ng2.ts
@@ -1,12 +1,17 @@
-import { Component, Inject, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Inject, Input, Output, EventEmitter, forwardRef } from '@angular/core';
 
 import { defaultThemeToken } from '../componentsDefaultTheme';
+
+import { ValueAccessor, makeValueAccessorProvider } from '../input/valueAccessor';
 
 @Component({
 	selector: 'rlCheckbox',
 	template: require('./checkbox.ng2.html'),
+	providers: [makeValueAccessorProvider({
+		useExisting: forwardRef(() => CheckboxComponent)
+	})],
 })
-export class CheckboxComponent {
+export class CheckboxComponent extends ValueAccessor<boolean> {
 	@Input() value: boolean;
 	@Input() disabled: boolean;
 	@Input() active: boolean = true;
@@ -16,12 +21,18 @@ export class CheckboxComponent {
 	useDefaultTheme: boolean;
 
 	constructor( @Inject(defaultThemeToken) useDefaultTheme: boolean) {
+		super();
 		this.useDefaultTheme = useDefaultTheme;
+	}
+
+	writeValue(value: boolean): void {
+		this.value = value;
 	}
 
 	toggle(): void {
 		if (this.active && !this.disabled) {
 			this.value = !this.value;
+			this.triggerChange(this.value);
 			this.change.emit(this.value);
 		}
 	}
