ce071d1085daa4d7673eae6026a05271411d78cc
SamGraber <samgraber@sundialproperties.com>
2016-05-25 11:13:06 -0400

    The simple control-controlGroup relationship we're using for forms doesn't appear to tie the form 'valid' state with the controls. Instead we'll use a simple helper method to check the validity of all of the controls for now.
    Use an angular 2 version of the form service to get the first error message off the form and notify it on a validation failure. As before, we could consider changing this if we wanted to eventually aggregate the errors.
    
    One option would be to have the form service return an array of errors, and have the form notify each of them.

--- source/components/form/form.ng2.ts
+++ source/components/form/form.ng2.ts
@@ -1,8 +1,12 @@
-import { Component, ViewChild, Input } from '@angular/core';
+import { Component, ViewChild, Input, Inject } from '@angular/core';
 import { NgForm, ControlGroup, FormBuilder, FORM_DIRECTIVES } from '@angular/common';
 import { Observable } from 'rxjs';
 
+import { services, downgrade } from 'typescript-angular-utilities';
+import __notification = services.notification;
+
 import { IWaitValue } from '../busy/busy.ng2';
+import { FormService } from '../../services/form/form.service.ng2';
 
 export interface ISaveAction<T> {
 	(data: T): IWaitValue<T>;
@@ -16,10 +20,14 @@ export interface ISaveAction<T> {
 export class FormComponent {
 	@Input() save: ISaveAction<any>;
 
-	// @ViewChild('form') form: NgForm;
 	form: ControlGroup;
+	private notification: __notification.INotificationService;
+	private formService: FormService;
 
-	constructor() {
+	constructor( @Inject(__notification.notificationServiceToken) notification: __notification.INotificationService
+			, formService: FormService) {
+		this.notification = notification;
+		this.formService = formService;
 		this.form = new ControlGroup({});
 		if (!this.save) {
 			this.save = <ISaveAction>() => Promise.resolve();
@@ -27,10 +35,11 @@ export class FormComponent {
 	}
 
 	submit(): IWaitValue<any> {
-		if (this.form.valid) {
+		if (this.formService.isFormValid(this.form)) {
 			return this.save(this.form.value);
 		} else {
-			// show validation error
+			this.notification.warning(this.formService.getAggregateError(this.form));
+			return false;
 		}
 	}
 }
