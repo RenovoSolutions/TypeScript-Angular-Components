8a5a2f5831600a6242f14c7adfdddfb03a83e970
SamGraber <samgraber@sundialproperties.com>
2016-05-27 08:35:04 -0400

    Date filter doesn't have a module anymore.

--- source/components/messageLog/messageLog.module.ts
+++ source/components/messageLog/messageLog.module.ts
@@ -1,6 +1,7 @@
 import * as angular from 'angular';
 
-import { downgrade } from 'typescript-angular-utilities';
+import { downgrade as utilitiesDowngrade } from 'typescript-angular-utilities';
+import { moduleName as componentsDowngradeModule } from '../../componentsDowngrade';
 
 import { moduleName as jqueryModuleName } from '../../services/jquery/jquery.service';
 
@@ -17,14 +18,13 @@ import * as componentServices from '../../services/services.module';
 import __dialog = componentServices.dialog;
 
 import { moduleName as templateLoaderModule } from '../../services/templateLoader/templateLoader.service';
-import { moduleName as dateFilterModule } from '../../filters/date/date.filter';
 
 export * from './messageLog.service';
 export * from './messageLog.directive';
 
 export var moduleName: string = 'rl.ui.components.messageLog';
 
-angular.module(moduleName, [downgrade.moduleName, jqueryModuleName, templateLoaderModule, __dialog.moduleName, dateFilterModule])
+angular.module(moduleName, [utilitiesDowngrade.moduleName, componentsDowngradeModule, jqueryModuleName, templateLoaderModule, __dialog.moduleName])
 	.factory(factoryName, messageLogFactory)
 	.directive(directiveName, messageLog)
 	.controller(controllerName, MessageLogController)
