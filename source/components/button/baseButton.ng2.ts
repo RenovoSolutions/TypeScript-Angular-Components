2a81660f6c0c2c189824039b064e1c0e2d0be1c4
SamGraber <samgraber@sundialproperties.com>
2016-06-08 15:21:10 -0400

    Moved the default button type and button class prefix to constants.

--- source/components/button/baseButton.ng2.ts
+++ source/components/button/baseButton.ng2.ts
@@ -3,6 +3,9 @@ import { each } from 'lodash';
 
 export const baseInputs: string[] = ['type', 'disabled', 'size'];
 
+const buttonClassPrefix: string = 'btn-';
+const defaultButtonType: string = 'default';
+
 export class BaseButtonComponent {
 	type: string;
 	disabled: boolean;
@@ -10,17 +13,17 @@ export class BaseButtonComponent {
 
 	get configuredSize(): string {
 		return this.size != null && this.size !== ''
-			? 'btn-' + this.size
+			? buttonClassPrefix + this.size
 			: null;
 	}
 
 	get configuredTypes(): string {
-		let type: string = this.type || 'default';
+		let type: string = this.type || defaultButtonType;
 		let typesList: string[] = type.split(' ');
 		each(typesList, (type: string, index: number): void => {
 			//the for each for places that used btn-block for example in the type attribute do not break
-			if (type.indexOf('btn-') === -1) {
-				type = 'btn-' + type;
+			if (type.indexOf(buttonClassPrefix) === -1) {
+				type = buttonClassPrefix + type;
 			}
 			typesList[index] = type;
 		});
