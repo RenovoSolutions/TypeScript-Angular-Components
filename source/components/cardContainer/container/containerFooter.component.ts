acea004145d471d8c41ba38fb8fc7d22c0c7adc9
SamGraber <samgraber@sundialproperties.com>
2016-06-20 10:45:26 -0400

    Header and footer corrections
    Depend on the correct directives. The html elements should match the selectors.

--- source/components/cardContainer/container/containerFooter.component.ts
+++ source/components/cardContainer/container/containerFooter.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 
 import { ContainerFooterTemplate } from './containerFooter.template';
 import { TemplateRenderer } from '../../templateRenderer/templateRenderer';
+import { ItemCountComponent } from './itemCount/itemCount';
 
 @Component({
 	selector: 'rlContainerFooter',
 	template: require('./containerFooter.component.html'),
-	directives: [TemplateRenderer],
+	directives: [TemplateRenderer, ItemCountComponent],
 })
 export class ContainerFooterComponent {
 	@Input() footer: ContainerFooterTemplate;
--- source/components/cardContainer/container/containerHeader.component.ts
+++ source/components/cardContainer/container/containerHeader.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 
 import { ContainerHeaderTemplate } from './containerHeader.template';
 import { TemplateRenderer } from '../../templateRenderer/templateRenderer';
+import { CardSearchComponent } from './cardSearch/cardSearch';
+import { PageSizeComponent } from '../paging/index';
 
 @Component({
 	selector: 'rlContainerHeader',
 	template: require('./containerHeader.component.html'),
-	directives: [TemplateRenderer],
+	directives: [TemplateRenderer, CardSearchComponent, PageSizeComponent],
 })
 export class ContainerHeaderComponent {
 	@Input() header: ContainerHeaderTemplate;
