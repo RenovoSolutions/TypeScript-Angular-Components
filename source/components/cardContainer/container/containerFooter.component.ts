8760606f5db96ac3493087251b3a2ffae5236b50
SamGraber <samgraber@sundialproperties.com>
2016-06-20 15:29:14 -0400

    Added the pager to the default footer
    I missed this in the ng1 default footer. The pager should be in the right 6 cols, pulled to the right. Using a forward ref right now due to an issue with circular references.

--- source/components/cardContainer/container/containerFooter.component.ts
+++ source/components/cardContainer/container/containerFooter.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
 
 import { ContainerFooterTemplate } from '../templates/containerFooter.template';
 import { ItemCountComponent } from './itemCount/itemCount';
+import { PagerComponent } from '../paging/pager/pager';
 
 @Component({
 	selector: 'rlContainerFooter',
 	template: require('./containerFooter.component.html'),
-	directives: [ItemCountComponent],
+	directives: [ItemCountComponent, forwardRef(() => PagerComponent)],
 })
 export class ContainerFooterComponent {
 	@Input() footer: ContainerFooterTemplate;
