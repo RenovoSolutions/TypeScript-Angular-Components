ff5768458dacd0b18991838364a63101b27e6d65
SamGraber <samgraber@sundialproperties.com>
2016-06-20 11:35:17 -0400

    Added an initial index for the card directives

---
+++ source/components/cardContainer/card/index.ts
@@ -0,0 +1,5 @@
+import { CARD_CONTENT_DIRECTIVES } from './content/index';
+
+export const CARD_DIRECTIVES: any[] = [CARD_CONTENT_DIRECTIVES];
+
+export * from './content/index';
--- source/components/cardContainer/index.ts
+++ source/components/cardContainer/index.ts
@@ -1,12 +1,13 @@
+import * as card from './card/index';
 import * as container from './container/index';
 import * as dataSources from './dataSources/index';
 import * as filters from './filters/index';
 import * as paging from './paging/index';
 import * as sorts from './sorts/index';
 
-export const CARD_CONTAINER_DIRECTIVES: any[] = [container.CONTAINER_DIRECTIVES, filters.FILTER_DIRECTIVES];
+export const CARD_CONTAINER_DIRECTIVES: any[] = [card.CARD_DIRECTIVES, container.CONTAINER_DIRECTIVES, filters.FILTER_DIRECTIVES];
 
-export { container, dataSources, filters, paging, sorts };
+export { card, container, dataSources, filters, paging, sorts };
 
 // card
 export * from './cardContainer';
