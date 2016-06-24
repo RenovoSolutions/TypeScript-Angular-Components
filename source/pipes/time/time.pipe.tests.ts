30d4ed651eddb7a811aff07f0abd2b1c72fd6ca8
SamGraber <samgraber@sundialproperties.com>
2016-06-24 16:32:45 -0400

    Time pipe test fixes
    Need to set a timezone. Use mountain time to ensure we can reproduce test failures locally.

--- source/pipes/time/time.pipe.tests.ts
+++ source/pipes/time/time.pipe.tests.ts
@@ -17,8 +17,8 @@ describe('TimePipe', () => {
 	});
 
 	it('should return just the time portion of the date', (): void => {
-		expect(timePipe.transform(moment('2016-01-02T12:00:00.000-05:00'))).to.equal('12:00PM');
-		expect(timePipe.transform(moment('2016-01-02T08:00:00.000-05:00'))).to.equal('8:00AM');
-		expect(timePipe.transform(moment('2016-01-02T20:00:00.000-05:00'))).to.equal('8:00PM');
+		expect(timePipe.transform(moment.tz('2016-01-02T12:00:00.000-06:00', 'US/Central'))).to.equal('12:00PM');
+		expect(timePipe.transform(moment.tz('2016-01-02T08:00:00.000-06:00', 'US/Central'))).to.equal('8:00AM');
+		expect(timePipe.transform(moment.tz('2016-01-02T20:00:00.000-06:00', 'US/Central'))).to.equal('8:00PM');
 	});
 });
