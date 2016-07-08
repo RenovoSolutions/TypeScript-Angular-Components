b3faecaab8e41bcef9bdcb55030e506947e81c7d
SamGraber <samgraber@sundialproperties.com>
2016-07-08 10:52:42 -0400

    Remove the dialog open button and demonstrate opening the dialog from the consumer

--- bootstrapper/popup/popupNg2Bootstrapper.ts
+++ bootstrapper/popup/popupNg2Bootstrapper.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TOOLTIP_DIRECTIVES } from 'ng2-bootstrap';
 
+import { ButtonComponent } from '../../source/components/buttons/index';
 import { DialogOutletComponent } from '../../source/components/dialog/dialogOutlet';
 import { DialogComponent } from '../../source/components/dialog/dialog';
 import { IDialogClosingHandler } from '../../source/components/dialog/dialogRoot.service';
@@ -11,6 +12,7 @@ import { DIALOG_TEMPLATE_DIRECTIVES } from '../../source/components/dialog/templ
 	template: require('./popupNg2.html'),
 	directives: [
 		TOOLTIP_DIRECTIVES,
+		ButtonComponent,
 		DIALOG_TEMPLATE_DIRECTIVES,
 		DialogOutletComponent,
 		DialogComponent,
