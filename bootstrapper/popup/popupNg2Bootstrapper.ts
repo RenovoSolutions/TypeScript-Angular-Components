f55e2f367818352e66c43debe2a8ea8f84cd973a
SamGraber <samgraber@sundialproperties.com>
2016-07-08 09:51:31 -0400

    Created a dialog outlet with a sample bootstrap dialog

--- bootstrapper/popup/popupNg2Bootstrapper.ts
+++ bootstrapper/popup/popupNg2Bootstrapper.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { TOOLTIP_DIRECTIVES } from 'ng2-bootstrap';
 
+import { DialogOutletComponent } from '../../source/components/dialog/dialogOutlet';
+import {} from '../../source/components/dialog/dialog';
+
 @Component({
 	selector: 'tsPopupBootstrapper',
 	template: require('./popupNg2.html'),
-	directives: [TOOLTIP_DIRECTIVES],
+	directives: [
+		TOOLTIP_DIRECTIVES,
+		DialogOutletComponent,
+	],
 })
 export class PopupBootstrapper {
 	content: string = 'Some content';
--- source/components/dialog/dialogOutlet.ts
+++ source/components/dialog/dialogOutlet.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'rlDialogOutlet',
+	template: require('./dialogOutlet.html'),
 })
 export class DialogOutletComponent {
 
