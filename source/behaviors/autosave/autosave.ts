9cc4d3c5358acf76b7f0ba3867090809cb206335
SamGraber <samgraber@sundialproperties.com>
2016-07-19 14:33:29 -0400

    On a status change, initiate an autosave cycle
    If the form is valid, the timer is set. This is canceled and reset on a keyup to 'debounce' the save. Also, if the form becomes invalid on a keyup, the autosave will be canceled altogether.

--- source/behaviors/autosave/autosave.ts
+++ source/behaviors/autosave/autosave.ts
@@ -1,15 +1,49 @@
-import { Directive, Self } from '@angular/core';
+import { Directive, Self, AfterViewInit, HostListener } from '@angular/core';
+
+import { services } from 'typescript-angular-utilities';
+import __timeout = services.timeout;
 
 import { FormComponent } from '../../components/form/form';
+
+const DEFAULT_AUTOSAVE_DEBOUNCE: number = 3000;
 
 @Directive({
 	selector: '[rlAutosave]',
 })
-export class AutosaveDirective {
-	form: FormComponent;
+export class AutosaveDirective implements AfterViewInit {
+	@HostListener('keyup') keyupListener = this.resetDebounce;
 
-	constructor( @Self() form: FormComponent) {
+	timer: __timeout.ITimeout;
+	form: FormComponent;
+	timeoutService: __timeout.TimeoutService;
+
+	constructor( @Self() form: FormComponent
+			, timeoutService: __timeout.TimeoutService) {
 		this.form = form;
-		form.form.statusChanges.subscribe(value => console.log(value));
+		this.timeoutService = timeoutService;
+	}
+
+	ngAfterViewInit(): void {
+		this.form.form.statusChanges.subscribe(this.setDebounce);
+	}
+
+	setDebounce = (): void => {
+		if (this.form.validate()) {
+			this.timer = this.timeoutService.setTimeout(this.autosave, DEFAULT_AUTOSAVE_DEBOUNCE)
+											.catch(() => null);
+		}
+	}
+
+	resetDebounce(): void {
+		if (this.timer) {
+			this.timer.cancel();
+			this.timer = null;
+			this.setDebounce();
+		}
+	}
+
+	autosave = (): void => {
+		console.log('Autosave');
+		this.timer = null;
 	}
 }
