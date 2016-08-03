7c7fd05236f025e1228fef29db6eb3f26f9b68b5
ibedard16 <ibedard@biomedscienceacademy.org>
2016-08-03 15:55:15 -0400

    Limit page size options again
    Firefox still crashing

--- source/components/cardContainer/paging/pageSize/pageSize.ts
+++ source/components/cardContainer/paging/pageSize/pageSize.ts
@@ -4,7 +4,7 @@ import { IDataPager } from '../dataPager/dataPager.service';
 import { CardContainerComponent } from '../../cardContainer';
 import { SelectComponent } from '../../../inputs/index';
 
-export const availablePageSizes: number[] = [10, 25, 50];
+export const availablePageSizes: number[] = [10, 15, 20, 25];
 
 @Component({
 	selector: 'rlPageSize',
