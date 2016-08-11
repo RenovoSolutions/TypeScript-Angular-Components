4f9108aa54179ce79630d613d88cc95071992b5d
SamGraber <samgraber@sundialproperties.com>
2016-08-11 12:15:47 -0400

    Implement the angular component router
    Strip out the old modules for now and bootstrap an angular 2 application. This is key for allowing us to setup the component router. Configured initial routes for inputs and misc.

--- bootstrapper/app.ts
+++ bootstrapper/app.ts
@@ -3,9 +3,10 @@ import 'bootstrap';
 import * as angular from 'angular';
 import 'angular-ui-router';
 
-import { NgModule, forwardRef } from '@angular/core';
+import { NgModule, forwardRef, ApplicationRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UpgradeAdapter } from '@angular/upgrade';
+import { Routes, RouterModule } from '@angular/router';
 
 import { TOOLTIP_DIRECTIVES } from 'ng2-bootstrap';
 
@@ -32,6 +33,7 @@ import { MsiBootstrapperComponent } from './msi/msiBootstrapper.ng2';
 import { CardsBootstrapper } from './cards/cardsNg2Bootstrapper';
 import { PopupBootstrapper } from './popup/popupNg2Bootstrapper';
 import { MiscNgContextBootstrapper } from './misc/miscNg2Context';
+import { App } from './app.ng2';
 
 import { INPUT_DIRECTIVES } from '../source/components/inputs/index';
 import { BUTTON_DIRECTIVES } from '../source/components/buttons/index';
@@ -58,29 +60,51 @@ const moduleName: string = 'bootstrapper-app';
 
 angular.module(moduleName, [
 	componentsModule,
-	'ui.router',
+	// 'ui.router',
 
 	utilitiesDowngrade.moduleName,
-	multStepModuleName,
-	inputModuleName,
-	buttonModuleName,
-	popupModuleName,
-	messageLogModuleName,
-	cardModuleName,
-	tabModuleName,
-	formModuleName,
-	miscModuleName,
-	textModuleName,
+	// multStepModuleName,
+	// inputModuleName,
+	// buttonModuleName,
+	// popupModuleName,
+	// messageLogModuleName,
+	// cardModuleName,
+	// tabModuleName,
+	// formModuleName,
+	// miscModuleName,
+	// textModuleName,
 ])
 	.component('tsBootstrapper', bootstrapper)
+	.directive('tsApp', <any>upgradeAdapter.downgradeNg2Component(App))
+	// .directive('tsRouterOutlet', <any>upgradeAdapter.downgradeNg2Component(RouterOutletComponent))
 	.directive('tsInputsBootstrapper', <any>upgradeAdapter.downgradeNg2Component(InputsBootstrapper))
 	.directive('tsFormsBootstrapper', <any>upgradeAdapter.downgradeNg2Component(FormsBootstrapper))
 	.directive('tsTabsBootstrapper', <any>upgradeAdapter.downgradeNg2Component(TabsBootstrapper))
 	.directive('tsMsiBootstrapper', <any>upgradeAdapter.downgradeNg2Component(MsiBootstrapperComponent))
 	.directive('tsCardsBootstrapper', <any>upgradeAdapter.downgradeNg2Component(CardsBootstrapper))
 	.directive('tsPopupBootstrapper', <any>upgradeAdapter.downgradeNg2Component(PopupBootstrapper))
-	.directive('tsMiscNgContext', <any>upgradeAdapter.downgradeNg2Component(MiscNgContextBootstrapper))
-	.config(BaseRoute);
+	.directive('tsMiscNgContext', <any>upgradeAdapter.downgradeNg2Component(MiscNgContextBootstrapper));
+	// .config(BaseRoute);
+
+const appRoutes: Routes = [
+	{ path: 'inputs', component: InputsBootstrapper },
+	{ path: 'misc', component: MiscNgContextBootstrapper },
+	// {
+	// 	path: 'heroes',
+	// 	component: HeroListComponent,
+	// 	data: {
+	// 		title: 'Heroes List'
+	// 	}
+	// },
+	// { path: 'hero/:id', component: HeroDetailComponent },
+	// { path: '**', component: PageNotFoundComponent }
+];
+
+export const appRoutingProviders: any[] = [
+
+];
+
+// export const routing = RouterModule.forRoot(appRoutes);
 
 @NgModule({
 	declarations: [
@@ -97,21 +121,33 @@ angular.module(moduleName, [
 		RatingBarComponent,
 		MultiStepIndicatorComponent,
 
+		InputsBootstrapper,
+		MiscNgContextBootstrapper,
+
 		// should be a module import eventually
 		TOOLTIP_DIRECTIVES,
 	],
-	imports: [BrowserModule]
+	providers: [
+		{
+			provide: ApplicationRef,
+			useValue: {
+				componentTypes: [App],
+				registerDisposeListener: () => {},
+			},
+		},
+	],
+	imports: [BrowserModule, RouterModule.forRoot(appRoutes)]
 })
 class ComponentsBootstrapperModule {}
 
-BaseRoute.$inject = ['$urlRouterProvider', '$stateProvider'];
-function BaseRoute($urlRouterProvider, $stateProvider) {
-	$urlRouterProvider.otherwise('/');
-	$stateProvider
-		.state('/', {
-			url: '/',
-			template: '<h3>Welcome to typescript-angular-components</h3>',
-		});
-}
+// BaseRoute.$inject = ['$urlRouterProvider', '$stateProvider'];
+// function BaseRoute($urlRouterProvider, $stateProvider) {
+// 	$urlRouterProvider.otherwise('/');
+// 	$stateProvider
+// 		.state('/', {
+// 			url: '/',
+// 			template: '<h3>Welcome to typescript-angular-components</h3>',
+// 		});
+// }
 
 upgradeAdapter.bootstrap(document.body, [moduleName]);
