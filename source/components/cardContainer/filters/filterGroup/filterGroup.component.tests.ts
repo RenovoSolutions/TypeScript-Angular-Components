75098af439836f84475c7ab668621af69c12f81f
arknotts <arknotts@gmail.com>
2016-08-11 16:51:25 -0400

    Add enabled to ng2 filter group.
    Same renaming and refactoring as previous ng1 commit.

--- source/components/cardContainer/filters/filterGroup/filterGroup.component.tests.ts
+++ source/components/cardContainer/filters/filterGroup/filterGroup.component.tests.ts
@@ -1,34 +1,68 @@
 import { FilterGroupComponent } from './filterGroup.component';
 
 describe('FilterGroupComponent', (): void => {
-	let filterGroup: FilterGroupComponent<any>;
+	let filterGroupComponent: FilterGroupComponent<any>;
+	let filterGroup = <any> {
+		label: 'some label',
+		activeOption: {
+			label: 'active option label'
+		}
+	};
 
 	beforeEach(() => {
-		filterGroup = new FilterGroupComponent(<any>{ log: sinon.spy() });
-		filterGroup.filterGroup = <any>{};
+		filterGroupComponent = new FilterGroupComponent(<any>{ log: sinon.spy() });
+		filterGroupComponent.filterGroup = filterGroup;
 	});
 
 	it('should toggle the children', (): void => {
-		expect(filterGroup.showChildren).to.be.true;
+		expect(filterGroupComponent.expanded).to.be.true;
 
-		filterGroup.toggleChildren();
+		filterGroupComponent.toggleExpanded();
 
-		expect(filterGroup.showChildren).to.be.false;
+		expect(filterGroupComponent.expanded).to.be.false;
 
-		filterGroup.toggleChildren();
+		filterGroupComponent.toggleExpanded();
 
-		expect(filterGroup.showChildren).to.be.true;
+		expect(filterGroupComponent.expanded).to.be.true;
+	});
+
+	it('should only show children if expanded and enabled', (): void => {
+		filterGroupComponent.expanded = true;
+		filterGroupComponent.enabled = false;
+		expect(filterGroupComponent.childrenVisible).to.be.false;
+
+		filterGroupComponent.expanded = false;
+		filterGroupComponent.enabled = true;
+		expect(filterGroupComponent.childrenVisible).to.be.false;
+
+		filterGroupComponent.expanded = false;
+		filterGroupComponent.enabled = false;
+		expect(filterGroupComponent.childrenVisible).to.be.false;
+
+		filterGroupComponent.expanded = true;
+		filterGroupComponent.enabled = true;
+		expect(filterGroupComponent.childrenVisible).to.be.true;
 	});
 
 	it('should set the active option and refresh the data source', (): void => {
 		const dataSource: any = { refresh: sinon.spy() };
-		filterGroup.dataSource = dataSource;
+		filterGroupComponent.dataSource = dataSource;
 		const option: any = { prop: 4 };
 
-		filterGroup.selectOption(option);
+		filterGroupComponent.selectOption(option);
 
 		sinon.assert.calledOnce(dataSource.refresh);
-		expect(filterGroup.filterGroup.activeOption).to.equal(option);
-		expect(filterGroup.showChildren).to.be.false;
+		expect(filterGroupComponent.filterGroup.activeOption).to.equal(option);
+		expect(filterGroupComponent.expanded).to.be.false;
+	});
+
+	it('should only show active option label if enabled', (): void => {
+		filterGroupComponent.enabled = true;
+		expect(filterGroupComponent.headerTitle).to.contain(filterGroup.label);
+		expect(filterGroupComponent.headerTitle).to.contain(filterGroup.activeOption.label);
+
+		filterGroupComponent.enabled = false;
+		expect(filterGroupComponent.headerTitle).to.contain(filterGroup.label);
+		expect(filterGroupComponent.headerTitle).to.not.contain(filterGroup.activeOption.label);
 	});
 });
