7f372c023a6a61bc6fe2205ce4442436d77c1abe
arknotts <arknotts@gmail.com>
2016-08-17 15:28:01 -0400

    Add rl-focus-on directive.
    This directive focuses an input given an expression to watch. It will focus the input the attribute is added to, else the first descendant input of the children tree.

---
+++ source/behaviors/focusOn/focusOn.ng1.ts
@@ -0,0 +1,29 @@
+import * as angular from 'angular';
+
+export const moduleName: string = 'rl.ui.behaviors.focusOn';
+export const directiveName: string = 'rlFocusOn';
+
+export function focusOn($timeout, $parse): angular.IDirective {
+	return {
+		link: function(scope, element, attrs: any) {
+			var model = $parse(attrs.rlFocusOn);
+			scope.$watch(model, function (value) {
+				if (value === true) {
+					$timeout(function () {
+						let thisElement = element[0];
+						if (thisElement.tagName.toLowerCase() == 'input') {
+							thisElement.focus();
+						}
+						else {
+							let ngElement = angular.element(thisElement);
+							ngElement.find('input').focus();
+						}
+					});
+				}
+			});
+		}
+	};
+}
+
+angular.module(moduleName, [])
+	.directive(directiveName, focusOn);
