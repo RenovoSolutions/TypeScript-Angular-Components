0c115bf84177bbf0eb06a71b4969b50861df8c6c
SamGraber <authorsamgraber@gmail.com>
2016-08-30 13:23:24 -0400

    Added the template directives to the dialog directive list
    These are needed for specifying dialog content. Consumers should get everything they need when they specify the dialog directives.

--- source/components/dialog/index.ts
+++ source/components/dialog/index.ts
@@ -2,8 +2,9 @@ import { DialogComponent } from './dialog';
 import { DialogOutletComponent } from './dialogOutlet';
 import { PromptDialogComponent } from './promptDialog';
 import { DialogRootService } from './dialogRoot.service';
+import { DIALOG_TEMPLATE_DIRECTIVES } from './templates/index';
 
-export const DIALOG_DIRECTIVES: any[] = [DialogComponent, DialogOutletComponent, PromptDialogComponent];
+export const DIALOG_DIRECTIVES: any[] = [DialogComponent, DialogOutletComponent, PromptDialogComponent, DIALOG_TEMPLATE_DIRECTIVES];
 export const DIALOG_PROVIDERS: any[] = [DialogRootService];
 
 export * from './dialog';
