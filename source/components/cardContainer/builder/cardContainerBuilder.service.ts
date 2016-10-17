ac8dfd2b83e26526d1917685fbfcceb10c2fd703
SamGraber <authorsamgraber@gmail.com>
2016-10-17 16:16:09 -0400

    Created an initial version of the new builder
    The new builder will act as a factory for card container 'instances' instead of an instance itself. This solves the issue where you couldn't include two card containers on the same page without cloning the builder. The 'getInstance' function will take most of the simple settings, leaving only the data source, filters, and columns to be specified individually.

---
+++ source/components/cardContainer/builder/cardContainerBuilder.service.ts
@@ -0,0 +1,62 @@
+import { Injector, Injectable } from '@angular/core';
+
+import { IColumn } from '../column';
+import { IDataSource, SimpleDataSource } from '../dataSources/index';
+import {} from '../paging/index';
+import { Sorter } from '../sorts/index';
+
+export enum CardContainerType {
+	standard,
+	selectable,
+}
+
+export interface ICardContainerProperties {
+	search?: boolean;
+	paging?: boolean;
+	maxColumnSorts?: number;
+	permanentFooters?: boolean;
+	disableSelection?: { (item: any): string };
+}
+
+export interface ICardContainerInstance { }
+
+export interface ICardContainerConstructor<TDataType> {
+	search?: boolean;
+	paging?: boolean;
+	maxColumnSorts?: number;
+	permanentFooters?: boolean;
+	disableSelection?: { (item: any): string };
+	columns: IColumn<TDataType>[];
+	dataSource: IDataSource<TDataType>;
+}
+
+@Injectable()
+export class CardContainerBuilderService {
+	private injector: Injector;
+
+	constructor(injector: Injector) {
+		this.injector = injector;
+	}
+
+	getInstance(settings: ICardContainerProperties): ICardContainerInstance {
+		return <any>{
+			search: settings.search,
+			paging: settings.paging,
+			maxColumnSorts: settings.maxColumnSorts,
+			permanentFooters: settings.permanentFooters,
+			disableSelection: settings.disableSelection,
+			columns: [],
+		};
+	}
+
+	addColumn<TItemType>(container: ICardContainerInstance, column: IColumn<TItemType>): void {
+		(container as ICardContainerConstructor<TItemType>).columns.push(column);
+	}
+
+	buildSimpleDataSource<TDataType>(container: ICardContainerInstance, data: TDataType[]): IDataSource<TDataType> {
+		const sorter: Sorter = this.injector.get(Sorter);
+		const dataSource = new SimpleDataSource(data, sorter);
+		(container as ICardContainerConstructor<TDataType>).dataSource = dataSource;
+		return dataSource;
+	}
+}
