dee63662edf5d28c0cbf487b39d7907227101818
Brent Miller <brentwmiller93@gmail.com>
2016-10-25 16:27:36 -0400

    Since we're using this rlStep component to build our MSI/tabs. Flexbox doesn't work quite right. The rlStep host element gets in the way.
    The only way I found that I could fix this is by binding the classes needed for the MSI to display properly to the host, rlStep, element and dropping the list item.

--- source/components/multiStepIndicator/step.component.ts
+++ source/components/multiStepIndicator/step.component.ts
@@ -9,18 +9,35 @@ export class StepComponent {
 	@Input() title: string;
 	@Input() link: any[] | string;
 	@Input() valid: boolean;
+	@Input() current: boolean;
 	@Input() useMsiStyling: boolean = false;
 
+	// Default classes
+	@HostBinding('class.active') hasActive: boolean = true;
+
+	// Conditional classes
 	@HostBinding('class.rl-multi-step-item') useMsi: boolean = false;
 	@HostBinding('class.rl-tab-item') useTab: boolean = false;
+	@HostBinding('class.error') hasError: boolean = false;
 
 	ngOnInit() {
 		this.setStepType();
+		this.checkIfValid();
+	}
+
+	ngOnChanges() {
+		this.checkIfValid();
 	}
 
 	setStepType(): boolean {
 		return this.useMsiStyling
 			? this.useMsi = true
 			: this.useTab = true;
+	}
+
+	checkIfValid() {
+		this.valid
+			? this.hasError = false
+			: this.hasError = true;
 	}
 }
