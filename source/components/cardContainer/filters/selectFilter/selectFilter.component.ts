12d4aa4f8999b5d5420da7acf891f2ad2175287b
SamGraber <authorsamgraber@gmail.com>
2016-10-31 13:13:33 -0400

    Updated the select filter component to work with the new select filter service
    The component is now exclusively just a pass-through for the service, so there is no need for testing anymore.

--- source/components/cardContainer/filters/selectFilter/selectFilter.component.ts
+++ source/components/cardContainer/filters/selectFilter/selectFilter.component.ts
@@ -2,39 +2,21 @@ import { Component, Input, ContentChild, TemplateRef } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { services } from 'typescript-angular-utilities';
-import __transform = services.transform;
-import __logger = services.logger;
+import ITransform = services.transform.ITransform;
 
-import { ISelectFilterOld } from './selectFilterOld.service';
-import { IDataSourceOld } from '../../datasources/dataSource';
+import { SelectFilter } from './selectFilter.service';
+import { IDataSource } from '../../datasources/dataSource';
 
 @Component({
 	selector: 'rlSelectFilter',
 	template: require('./selectFilter.html'),
 })
 export class SelectFilterComponent<T> {
-	@Input() filter: ISelectFilterOld<T>;
-	@Input() dataSource: IDataSourceOld<T>;
+	@Input() filter: SelectFilter<T, any>;
 	@Input() label: string;
-	@Input() options: T[] | Observable<T[]>;
-	@Input() transform: __transform.ITransform<T, string>;
+	@Input() options: T[];
+	@Input() transform: ITransform<T, string>;
 	@Input() nullOption: string;
 
 	@ContentChild(TemplateRef) template: TemplateRef<any>;
-
-	logger: __logger.ILogger;
-
-	constructor(logger: __logger.Logger) {
-		this.logger = logger;
-	}
-
-	setValue(value: T): void {
-		this.filter.selectedValue = value;
-
-		if (this.dataSource != null) {
-			this.dataSource.refresh();
-		} else {
-			this.logger.log('No source specified');
-		}
-	}
 }
