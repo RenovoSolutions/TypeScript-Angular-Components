e48c7bd643906f23397b5d8174a435f67580c1b7
SamGraber <authorsamgraber@gmail.com>
2016-10-31 14:37:53 -0400

    Date filter component test fixes
    Instead of trying to rely on the concrete implementation of the date filter service (which was broken because we weren't using it here anyway), we'll just spy on the set functions for dateFrom and dateTo.

--- source/components/cardContainer/filters/dateFilter/dateFilter.component.tests.ts
+++ source/components/cardContainer/filters/dateFilter/dateFilter.component.tests.ts
@@ -1,4 +1,6 @@
+import { Observable } from 'rxjs';
 import * as moment from 'moment';
+import { rlFakeAsync, rlTick } from 'rl-async-testing';
 
 import { services } from 'typescript-angular-utilities';
 import __date = services.date;
@@ -9,12 +11,23 @@ interface ITestObj {
 	value: moment.Moment;
 }
 
+interface IDateFilterMock {
+	setDateFrom: Sinon.SinonSpy;
+	setDateTo: Sinon.SinonSpy;
+	dateFrom$?: Observable<moment.Moment>;
+}
+
 describe('DateFilterComponent', (): void => {
 	let dateFilter: DateFilterComponent<any>;
+	let filterService: IDateFilterMock;
 
 	beforeEach(() => {
 		dateFilter = new DateFilterComponent(__date.dateUtility);
-		dateFilter.filter = <any>{};
+		filterService = {
+			setDateFrom: sinon.spy(),
+			setDateTo: sinon.spy(),
+		}
+		dateFilter.filter = <any>filterService;
 	});
 
 	it('should set useTime on the filter', (): void => {
@@ -41,38 +54,30 @@ describe('DateFilterComponent', (): void => {
 		expect(dateFilter.showClear).to.be.false;
 	});
 
-	it('should set the date on the filter and refresh the data source', (): void => {
+	it('should set the date on the filter', (): void => {
 		const date: moment.Moment = moment('2000-01-01T05:16:00.000');
-		let dateFrom;
-		dateFilter.filter.dateFrom$.subscribe(value => dateFrom = value);
 
 		dateFilter.setDate(date);
 
-		expect(dateFrom).to.equal(date);
+		sinon.assert.calledOnce(filterService.setDateFrom);
+		sinon.assert.calledWith(filterService.setDateFrom, date);
 	});
 
-	it('should set the dateTo to a number days equal to the count before the dateFrom', (): void => {
+	it('should set the dateTo to a number days equal to the count before the dateFrom', rlFakeAsync((): void => {
 		const dateFrom: moment.Moment = moment('2000-01-05T05:16:00.000');
-		dateFilter.filter.setDateFrom(dateFrom);
-		let dateTo;
-		dateFilter.filter.dateTo$.subscribe(value => dateTo = value);
+		filterService.dateFrom$ = Observable.of(dateFrom);
 
 		dateFilter.setCount(4);
+		rlTick();
 
-		expect(dateTo).to.equalMoment(dateFrom.add('days', 4));
-	});
+		sinon.assert.calledOnce(filterService.setDateTo);
+		expect(filterService.setDateTo.firstCall.args[0]).to.equalMoment(moment('2000-01-01T05:16:00.000'));
+	}));
 
 	it('should clear the dateTo if the count is 0', (): void => {
-		const originalDateFrom: moment.Moment = moment('2000-01-05T05:16:00.000');
-		const originalDateTo: moment.Moment = moment('2000-01-01T05:16:00.000');
-		dateFilter.filter.setDateFrom(originalDateFrom);
-		dateFilter.filter.setDateTo(originalDateTo);
-		let dateTo;
-		dateFilter.filter.dateTo$.subscribe(value => dateTo = value);
-
 		dateFilter.setCount(0);
-
-		expect(dateTo).to.be.null;
+		sinon.assert.calledOnce(filterService.setDateTo);
+		sinon.assert.calledWith(filterService.setDateTo, null);
 	});
 
 	it('should set the date to null and count to 0', (): void => {
